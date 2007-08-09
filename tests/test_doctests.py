d63c67583f0ef8513d8b83c665226e3780dc79c4
dokai <dokai@b426a367-1105-0410-b9ff-cdf4ab011145>
2007-08-09 08:20:40 +0000

    Added an alternative doctest runner.
    git-svn-id: http://svn.gispython.org/svn/gispy/Shapely/trunk@808 b426a367-1105-0410-b9ff-cdf4ab011145

---
+++ tests/test_doctests.py
@@ -0,0 +1,31 @@
+import unittest
+import glob
+import os
+
+from zope.testing import doctest
+
+optionflags = (doctest.REPORT_ONLY_FIRST_FAILURE |
+               doctest.NORMALIZE_WHITESPACE |
+               doctest.ELLIPSIS)
+
+def list_doctests():
+    return [filename
+            for filename
+            in glob.glob(os.path.join(os.path.dirname(__file__), '*.txt'))]
+
+def open_file(filename, mode='r'):
+    """Helper function to open files from within the tests package."""
+    return open(os.path.join(os.path.dirname(__file__), filename), mode)
+
+def setUp(test):
+    test.globs.update(dict(
+            open_file = open_file,
+            ))
+
+def test_suite():
+    return unittest.TestSuite(
+        [doctest.DocFileSuite(os.path.basename(filename),
+                              optionflags=optionflags,
+                              setUp=setUp)
+         for filename
+         in list_doctests()])
