8700101212690d4e8a9b7b1362c027514186904e
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2007-09-13 03:48:04 +0000

    Remove needless try/except
    git-svn-id: http://svn.gispython.org/svn/gispy/Shapely/trunk@873 b426a367-1105-0410-b9ff-cdf4ab011145

--- shapely/geometry/geo.py
+++ shapely/geometry/geo.py
@@ -8,28 +8,28 @@ from multipolygon import MultiPolygonAdapter
 
 
 def asShape(context):
-    try:
-        if hasattr(context, "__geo_interface__"):
-            ob = context.__geo_interface__
-        else:
-            ob = context
+    """Adapts the context to a geometry interface. The coordinates remain
+    stored in the context.
+    """
+    if hasattr(context, "__geo_interface__"):
+        ob = context.__geo_interface__
+    else:
+        ob = context
 
-        geom_type = ob.get("type").lower()
+    geom_type = ob.get("type").lower()
 
-        if geom_type == "point":
-            return PointAdapter(ob["coordinates"])
-        elif geom_type == "linestring":
-            return LineStringAdapter(ob["coordinates"])
-        elif geom_type == "polygon":
-            return PolygonAdapter(ob["coordinates"][0], ob["coordinates"][1:])
-        elif geom_type == "multipoint":
-            return MultiPointAdapter(ob["coordinates"])
-        elif geom_type == "multilinestring":
-            return MultiLineStringAdapter(ob["coordinates"])
-        elif geom_type == "multipolygon":
-            return MultiPolygonAdapter(ob["coordinates"])
-        else:
-            raise ValueError, "Unknown geometry type: %s" % geom_type
-    except:
-        raise
+    if geom_type == "point":
+        return PointAdapter(ob["coordinates"])
+    elif geom_type == "linestring":
+        return LineStringAdapter(ob["coordinates"])
+    elif geom_type == "polygon":
+        return PolygonAdapter(ob["coordinates"][0], ob["coordinates"][1:])
+    elif geom_type == "multipoint":
+        return MultiPointAdapter(ob["coordinates"])
+    elif geom_type == "multilinestring":
+        return MultiLineStringAdapter(ob["coordinates"])
+    elif geom_type == "multipolygon":
+        return MultiPolygonAdapter(ob["coordinates"])
+    else:
+        raise ValueError, "Unknown geometry type: %s" % geom_type
 
