efefe2b329f56c1c40066bac6f34ec95cec5e373
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2007-10-13 14:30:47 +0000

    Increment version to 1.0a6
    git-svn-id: http://svn.gispython.org/svn/gispy/Shapely/trunk@903 b426a367-1105-0410-b9ff-cdf4ab011145

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, Extension
 readme_text = file('README.txt', 'rb').read()
 
 setup(name          = 'Shapely',
-      version       = '1.0a5',
+      version       = '1.0a6',
       description   = 'Geospatial geometries, predicates, and operations',
       license       = 'LGPL',
       keywords      = 'geometry topology',
