695ee2d009546e1991e475cc6947663222cb8b15
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2008-11-17 21:56:06 +0000

    Remove get of WKB dims
    git-svn-id: http://svn.gispython.org/svn/gispy/Shapely/trunk@1153 b426a367-1105-0410-b9ff-cdf4ab011145

--- shapely/ctypes_declarations.py
+++ shapely/ctypes_declarations.py
@@ -14,8 +14,6 @@ lgeos.GEOSGeomFromWKT.argtypes = [ctypes.c_char_p]
 lgeos.GEOSGeomToWKT.restype = ctypes.c_char_p
 lgeos.GEOSGeomToWKT.argtypes = [ctypes.c_void_p]
 
-lgeos.GEOS_getWKBOutputDims.restype = ctypes.c_int
-
 lgeos.GEOS_setWKBOutputDims.restype = ctypes.c_int
 lgeos.GEOS_setWKBOutputDims.argtypes = [ctypes.c_int]
 
