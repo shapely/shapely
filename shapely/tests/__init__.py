25f26842b8371b13b3fc9f4abf12dfba0b0408bc
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2009-12-06 16:03:14 +0000

    Integrate tests of prepared geoms into main test suite.
    git-svn-id: http://svn.gispython.org/svn/gispy/Shapely/branches/1.2@1508 b426a367-1105-0410-b9ff-cdf4ab011145

--- shapely/tests/__init__.py
+++ shapely/tests/__init__.py
@@ -1,2 +1,10 @@
-# package
-from test_doctests import test_suite
+from unittest import TestSuite
+
+import test_doctests, test_prepared
+
+def test_suite():
+    suite = TestSuite()
+    suite.addTest(test_doctests.test_suite())
+    suite.addTest(test_prepared.test_suite())
+    return suite
+
