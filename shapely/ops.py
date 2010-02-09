9a216046017b8299ea1da6a4403bc748d45b0494
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2010-02-09 11:12:10 +0000

    Gain some initial separation between ops and their GEOS implementations.
    git-svn-id: http://svn.gispython.org/svn/gispy/Shapely/branches/1.2@1546 b426a367-1105-0410-b9ff-cdf4ab011145

--- shapely/ops.py
+++ shapely/ops.py
@@ -8,61 +8,72 @@ from shapely.geos import lgeos
 from shapely.geometry.base import geom_factory, BaseGeometry
 from shapely.geometry import asShape, asLineString, asMultiLineString
 
-def shapeup(ob):
-    if isinstance(ob, BaseGeometry):
-        return ob
-    else:
-        try:
-            return asShape(ob)
-        except ValueError:
-            return asLineString(ob)
 
-def polygonize(iterator):
-    """Creates polygons from a list of LineString objects.
-    """
-    lines = [shapeup(ob) for ob in iterator]
-    geom_array_type = c_void_p * len(lines)
-    geom_array = geom_array_type()
-    for i, line in enumerate(lines):
-        geom_array[i] = line._geom
-    product = lgeos.GEOSPolygonize(byref(geom_array), len(lines))
-    collection = geom_factory(product)
-    for g in collection.geoms:
-        clone = lgeos.GEOSGeom_clone(g._geom)
-        g = geom_factory(clone)
-        g._owned = False
-        yield g
+class CollectionOperator(object):
 
-def linemerge(lines): 
-    """Merges all connected lines. Returns a LineString or MultiLineString 
-    when lines are not contiguous. 
+    def shapeup(self, ob):
+        if isinstance(ob, BaseGeometry):
+            return ob
+        else:
+            try:
+                return asShape(ob)
+            except ValueError:
+                return asLineString(ob)
+
+    def polygonize(self, lines):
+        """Creates polygons from a source of lines
         
-    Parameters 
-    ---------- 
-    lines : MultiLineString, sequence of lines, or sequence of coordinates
-    """ 
-    multilinestring = None 
-    if hasattr(lines, 'type') and lines.type == 'MultiLineString': 
-        multilinestring = lines 
-    elif hasattr(lines, '__iter__'): 
-        try: 
-            multilinestring = asMultiLineString([ls.coords for ls in lines]) 
-        except AttributeError: 
-            multilinestring = asMultiLineString(lines) 
-    if multilinestring is None: 
-        raise ValueError("Cannot linemerge %s" % lines)
-    result = lgeos.GEOSLineMerge(multilinestring._geom) 
-    return geom_factory(result)   
+        The source may be a MultiLineString, a sequence of LineString objects,
+        or a sequence of objects than can be adapted to LineStrings.
+        """
+        source = getattr(lines, 'geoms', None) or lines
+        obs = [self.shapeup(l) for l in source]
+        geom_array_type = c_void_p * len(obs)
+        geom_array = geom_array_type()
+        for i, line in enumerate(obs):
+            geom_array[i] = line._geom
+        product = lgeos.GEOSPolygonize(byref(geom_array), len(obs))
+        collection = geom_factory(product)
+        for g in collection.geoms:
+            clone = lgeos.GEOSGeom_clone(g._geom)
+            g = geom_factory(clone)
+            g._owned = False
+            yield g
 
-def cascaded_union(geoms):
-    """Returns the union of a sequence of geometries
-    
-    This is the most efficient method of dissolving many polygons.
-    """
-    L = len(geoms)
-    subs = (c_void_p * L)()
-    for i, g in enumerate(geoms):
-        subs[i] = g._geom
-    collection = lgeos.GEOSGeom_createCollection(6, subs, L)
-    return geom_factory(lgeos.GEOSUnionCascaded(collection))
+    def linemerge(self, lines): 
+        """Merges all connected lines from a source
+        
+        The source may be a MultiLineString, a sequence of LineString objects,
+        or a sequence of objects than can be adapted to LineStrings.  Returns a
+        LineString or MultiLineString when lines are not contiguous. 
+        """ 
+        source = None 
+        if hasattr(lines, 'type') and lines.type == 'MultiLineString': 
+            source = lines 
+        elif hasattr(lines, '__iter__'): 
+            try: 
+                source = asMultiLineString([ls.coords for ls in lines]) 
+            except AttributeError: 
+                source = asMultiLineString(lines) 
+        if source is None: 
+            raise ValueError("Cannot linemerge %s" % lines)
+        result = lgeos.GEOSLineMerge(source._geom) 
+        return geom_factory(result)   
+
+    def cascaded_union(geoms):
+        """Returns the union of a sequence of geometries
+        
+        This is the most efficient method of dissolving many polygons.
+        """
+        L = len(geoms)
+        subs = (c_void_p * L)()
+        for i, g in enumerate(geoms):
+            subs[i] = g._geom
+        collection = lgeos.GEOSGeom_createCollection(6, subs, L)
+        return geom_factory(lgeos.GEOSUnionCascaded(collection))
+
+operator = CollectionOperator()
+polygonize = operator.polygonize
+linemerge = operator.linemerge
+cascaded_union = operator.cascaded_union
 
