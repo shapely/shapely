01577a5901eef5255484f964f0e1c3fd80426f80
sgillies <sean.gillies@gmail.com>
2010-09-19 09:48:10 -0600

    Fixes for problems found by Pietro when preparing Shapely for Debian.

--- shapely/tests/test_dlls.py
+++ shapely/tests/test_dlls.py
@@ -8,7 +8,10 @@ class LoadingTestCase(unittest.TestCase):
     def test_load(self):
         self.assertRaises(OSError, load_dll, 'geosh_c')
     def test_fallbacks(self):
-        a = load_dll('geosh_c', fallbacks=['/opt/local/lib/libgeos_c.dylib'])
+        a = load_dll('geosh_c', fallbacks=[
+            '/opt/local/lib/libgeos_c.dylib',
+            'libgeos_c.so.1', 
+            'libgeos_c.so'])
 
 def test_suite():
     return unittest.TestLoader().loadTestsFromTestCase(LoadingTestCase)
