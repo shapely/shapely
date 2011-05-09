e9b767a07ce56acc686450540dd0f8836d427b18
Oliver Tonnhofer <olt@bogosoft.com>
2011-05-09 19:42:13 +0200

    impove load_dll test on Mac OS X with homebrew

--- shapely/tests/test_dlls.py
+++ shapely/tests/test_dlls.py
@@ -7,8 +7,9 @@ class LoadingTestCase(unittest.TestCase):
         self.assertRaises(OSError, load_dll, 'geosh_c')
     def test_fallbacks(self):
         a = load_dll('geosh_c', fallbacks=[
-            '/opt/local/lib/libgeos_c.dylib',
-            'libgeos_c.so.1', 
+            '/opt/local/lib/libgeos_c.dylib', # MacPorts
+            '/usr/local/lib/libgeos_c.dylib', # homebrew (Mac OS X)
+            'libgeos_c.so.1',
             'libgeos_c.so'])
 
 def test_suite():
