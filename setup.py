c87cf701ed62fc5ac1b0230747ed08dd99399cfb
Oliver Tonnhofer <olt@bogosoft.com>
2011-05-15 19:04:43 +0200

    add shapely.speedups as package in setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,8 @@ setup_args = dict(
     maintainer_email    = 'sean.gillies@gmail.com',
     url                 = 'http://trac.gispython.org/lab/wiki/Shapely',
     long_description    = readme_text + "\n" + changes_text,
-    packages            = ['shapely', 'shapely.geometry', 'shapely.algorithms'],
+    packages            = ['shapely', 'shapely.geometry', 'shapely.algorithms',
+                           'shapely.speedups'],
     test_suite          = 'shapely.tests.test_suite',
     classifiers         = [
         'Development Status :: 5 - Production/Stable',
