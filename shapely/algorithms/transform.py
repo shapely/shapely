2e947ac401da3481ac8fc38d57343efe3a52fcfa
Mike Toews <mwtoews@gmail.com>
2012-11-02 14:44:44 +1300

    Added rotate, scale, skew and transform functions to transform module

--- shapely/algorithms/transform.py
+++ shapely/algorithms/transform.py
@@ -1,5 +1,7 @@
+from math import sin, cos, tan, pi
+
 def affine(geom, matrix):
-    """Perform an affine transformation on a geometry
+    """Return a transformed geometry using an affine transformation matrix
 
     The coefficient matrix is provided as a list or tuple with 6 or 12 items
     for 2D or 3D transformations, respectively.
@@ -9,7 +11,7 @@ def affine(geom, matrix):
     which represents the transformation matrix:
         / a  b  xoff \
         | d  e  yoff |
-        \ 0  0     1 /
+        \ 0  0    1  /
     where the vertices are transformed as follows:
         x' = a*x + b*y + xoff
         y' = d*x + e*y + yoff
@@ -17,16 +19,15 @@ def affine(geom, matrix):
     For 3D affine transformations, the 12 parameter matrix is:
         [a, b, c, d, e, f, g, h, i, xoff, yoff, zoff]
     which represents the transformation matrix:
-        / a  b  c xoff \
-        | d  e  f yoff |
-        | g  h  i zoff |
-        \ 0  0  0    1 /
+        / a  b  c  xoff \
+        | d  e  f  yoff |
+        | g  h  i  zoff |
+        \ 0  0  0    1  /
     where the vertices are transformed as follows:
         x' = a*x + b*y + c*z + xoff
         y' = d*x + e*y + f*z + yoff
         z' = g*x + h*y + i*z + zoff
     """
-    matrix = [float(x) for x in matrix]
     if len(matrix) == 6:
         ndim = 2
         a, b, d, e, xoff, yoff = matrix
@@ -67,3 +68,111 @@ def affine(geom, matrix):
     else:
         raise ValueError('Type %r not recognized'%geom.type)
 
+def rotate(geom, angle, origin=None, use_degrees=True):
+    """Return a rotated geometry on a 2D plane
+
+    The angle of rotation can be specified in either degrees (default) or
+    radians by setting `use_degrees=False`. Positive rotations are clockwise.
+
+    The point of origin can be specified as a Point, otherwise the default
+    origin is the centroid of the geometry.
+
+    The transformation matrix for 2D rotation is:
+        / cos(r) -sin(r)  xoff \
+        | sin(r)  cos(r)  yoff |
+        \   0       0       1  /
+    where the offsets are calculated from the origin Point(x0, y0):
+        xoff = x0 - cos(r) * x0 + sin(r) * y0
+        yoff = y0 - sin(r) * x0 - cos(r) * y0
+    """
+    if use_degrees:
+        angle *= pi/180.0
+    cosp = cos(angle)
+    sinp = sin(angle)
+    if abs(cosp) < 2e-16:
+        cosp = 0.0
+    if abs(sinp) < 2e-16:
+        sinp = 0.0
+    if origin is None:
+        origin = geom.centroid
+    elif not (hasattr(origin, 'type') and origin.type == 'Point'):
+        raise TypeError("'origin' must be either 'None' or a 'Point' of origin")
+    x0, y0 = origin.coords[0][0:2]
+    matrix = (cosp, -sinp, 0,
+              sinp,  cosp, 0,
+              0, 0, 1,
+              x0 - cosp * x0 + sinp * y0, y0 - sinp * x0 - cosp * y0, 0)
+    return affine(geom, matrix)
+
+def scale(geom, xfact=1.0, yfact=1.0, zfact=1.0, origin=None):
+    """Return a scaled geometry, scaled by factors in x, y and/or z directions
+
+    The point of origin can be specified as a Point, otherwise the default
+    origin is the centroid of the geometry.
+
+    The general 3D affine transformation matrix for scaling is:
+        / xfact  0    0    xoff \
+        |   0  yfact  0    yoff |
+        |   0    0  zfact  zoff |
+        \   0    0    0     1   /
+    where the offsets are calculated from the origin Point(x0, y0, z0):
+        xoff = x0 - x0 * xfact
+        yoff = y0 - y0 * yfact
+        zoff = z0 - z0 * zfact
+    """
+    if origin is None:
+        origin = geom.centroid
+    elif not (hasattr(origin, 'type') and origin.type == 'Point'):
+        raise TypeError("'origin' must be either 'None' or a 'Point' of origin")
+    if geom.has_z and origin.has_z:
+        x0, y0, z0 = origin.coords[0]
+    else:
+        x0, y0 = origin.coords[0][0:2]
+        z0 = 0.0
+    matrix = (xfact, 0.0, 0.0,
+              0.0, yfact, 0.0,
+              0.0, 0.0, zfact,
+              x0 - x0 * xfact, y0 - y0 * yfact, z0 - z0 * zfact)
+    return affine(geom, matrix)
+
+def skew(geom, xshear=0.0, yshear=0.0, origin=None, use_degrees=True):
+    """Return a skewed geometry, sheared by angles in x, and/or y directions
+
+    The shear angle can be specified in either degrees (default) or radians by
+    setting `use_degrees=False`.
+
+    The point of origin can be specified as a Point, otherwise the default
+    origin is the centroid of the geometry.
+
+    The general 2D affine transformation matrix for skewing is:
+        /   1    tan(xs) 0  xoff \
+        | tan(ys)  1     0  yoff |
+        \   0      0     0   1   /
+    where the offsets are calculated from the origin Point(x0, y0):
+        xoff = -x0 * tan(xs)
+        yoff = -y0 * tan(ys)
+    """
+    if use_degrees:
+        xshear *= pi/180.0
+        yshear *= pi/180.0
+    tanx = tan(xshear)
+    tany = tan(yshear)
+    if abs(tanx) < 2e-16:
+        tanx = 0.0
+    if abs(tany) < 2e-16:
+        tany = 0.0
+    if origin is None:
+        origin = geom.centroid
+    elif not (hasattr(origin, 'type') and origin.type == 'Point'):
+        raise TypeError("'origin' must be either 'None' or a 'Point' of origin")
+    x0, y0 = origin.coords[0][0:2]
+    matrix = (1.0, tanx, 0.0,
+              tany, 1.0, 0.0,
+              0.0, 0.0, 1.0,
+              -x0 * tanx, -y0 * tany, 0.0)
+    return affine(geom, matrix)
+
+def translate(geom, x=0.0, y=0.0, z=0.0):
+    """Return a translated geometry shifted by offsets along x, y and/or z"""
+    matrix = (1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, x, y, z)
+    return affine(geom, matrix)
