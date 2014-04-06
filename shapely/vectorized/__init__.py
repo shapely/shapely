cfd769cfff25021bcbc3a10a37325430ecbbb33f
Brandon Wood <btwood@geometeor.com>
2014-04-06 13:42:28 -0400

    ENH: Cleanup of vectorized contains code, performance enhancements.
    I removed the 2-dimensional version of `contains` and the new tests
    reflect this change as well.

--- shapely/vectorized/__init__.py
+++ shapely/vectorized/__init__.py
@@ -4,7 +4,7 @@
 # at _vectorized.pyx
 
 try:
-    from _vectorized import (contains, contains2d, )
+    from _vectorized import contains
 except ImportError:
     raise ImportError('The vectorized submodule is not been installed. '
                       'Cython is required at build time for vectorized '
