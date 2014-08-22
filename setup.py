f1df99c4da8348f9d3116db924ee370cb896914e
Jacob Wasserman <jwasserman@gmail.com>
2014-08-22 09:34:27 -0400

    Pass in speedups option to test from command line
    Allows testing speedups by forcing on or off from the command
    line as:
    python setup.py test --speedups=true
    or
    python setup.py test --speedups=false
    
    Travis is configured to have an environment variable to run it
    both ways

--- setup.py
+++ setup.py
@@ -27,10 +27,12 @@ import sys
 class test(Command):
     """Run unit tests after in-place build"""
     description = __doc__
-    user_options = []
+    user_options = [
+        ('speedups=', None, 'Specify whether to force speedups: true or false'),
+    ]
 
     def initialize_options(self):
-        pass
+        self.speedups = None
 
     def finalize_options(self):
         pass
@@ -51,14 +53,20 @@ class test(Command):
         else:
             from unittest import TextTestRunner, TestLoader
 
-        speedups_enabled = os.environ.get('SHAPELY_SPEEDUPS_ENABLED', None)
-        if speedups_enabled is not None:
+        # Handle if --speedups={true|false} specified on command line
+        if self.speedups is not None:
             import shapely.speedups
-            if speedups_enabled == 'TRUE' and shapely.speedups.available:
+            if self.speedups == 'true':
+                if not shapely.speedups.available:
+                    print("speedups are forced but unavailable")
+                    sys.exit(1)
                 shapely.speedups.enable()
-            elif speedups_enabled == 'FALSE':
+            elif self.speedups == 'false':
                 shapely.speedups.available = False
                 shapely.speedups.disable()
+            else:
+                print("Unrecognized speedups value: {}".format(self.speedups))
+                sys.exit(1)
         import shapely.tests
         tests = TestLoader().loadTestsFromName('test_suite', shapely.tests)
         runner = TextTestRunner(verbosity=2)
