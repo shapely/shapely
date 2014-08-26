5549de0f4df576f66fb9a472846aae890fbbafc0
Jacob Wasserman <jwasserman@gmail.com>
2014-08-26 10:09:45 -0400

    Force list from iterable in speedups.

--- shapely/speedups/_speedups.pyx
+++ shapely/speedups/_speedups.pyx
@@ -89,7 +89,11 @@ def geos_linestring_from_py(ob, update_geom=None, update_ndim=0):
 
     except AttributeError:
         # Fall back on list
-        m = len(ob)
+        try:
+            m = len(ob)
+        except TypeError:  # Iterators, e.g. Python 3 zip
+            ob = list(ob)
+            m = len(ob)
         if m < 2:
             raise ValueError(
                 "LineStrings must have at least 2 coordinate tuples")
@@ -224,7 +228,11 @@ def geos_linearring_from_py(ob, update_geom=None, update_ndim=0):
             
     except AttributeError:
         # Fall back on list
-        m = len(ob)
+        try:
+            m = len(ob)
+        except TypeError:  # Iterators, e.g. Python 3 zip
+            ob = list(ob)
+            m = len(ob)
         n = len(ob[0])
         if m < 3:
             raise ValueError(
