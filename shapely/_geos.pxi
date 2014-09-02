shapely/shapely

commit 45ee253db42616cc4dcf7ffa93010a0f2e039429

Extended geometry cloning for efficient LineString<->LinearRing conversion.
Added code to improve efficiency (using both ctypes and cython) where a
LineString is created from an LinearRing, or vice versa.

shapely/_geos.pxi
@@ -25,6 +25,7 @@ cdef extern from "geos_c.h":
     GEOSGeometry *GEOSGeom_createLineString_r(GEOSContextHandle_t, GEOSCoordSequence *) nogil
     GEOSGeometry *GEOSGeom_createLinearRing_r(GEOSContextHandle_t, GEOSCoordSequence *) nogil
     GEOSGeometry *GEOSGeom_clone_r(GEOSContextHandle_t, GEOSGeometry *) nogil
+    GEOSCoordSequence *GEOSCoordSeq_clone_r(GEOSContextHandle_t, GEOSCoordSequence *) nogil
     int GEOSGeom_getCoordinateDimension_r(GEOSContextHandle_t, GEOSGeometry *) nogil
 
     void GEOSGeom_destroy_r(GEOSContextHandle_t, GEOSGeometry *) nogil

shapely/geometry/polygon.py
@@ -345,8 +345,12 @@ def orient(polygon, sign=1.0):
 
 def geos_linearring_from_py(ob, update_geom=None, update_ndim=0):
     # If a LinearRing is passed in, clone it and return
-    if isinstance(ob, LinearRing):
-        return geos_geom_from_py(ob)
+    # If a LineString is passed in, clone the coord seq and return a LinearRing
+    if isinstance(ob, LineString):
+        if type(ob) == LinearRing:
+            return geos_geom_from_py(ob)
+        else:
+            return geos_geom_from_py(ob, lgeos.GEOSGeom_createLinearRing)
 
     # If numpy is present, we use numpy.require to ensure that we have a
     # C-continguous array that owns its data. View data will be copied.

shapely/speedups/_speedups.pyx
@@ -33,10 +33,16 @@ def geos_linestring_from_py(ob, update_geom=None, update_ndim=0):
     cdef int i, n, m, sm, sn
 
     # If a LineString is passed in, just clone it and return
+    # If a LinearRing is passed in, clone the coord seq and return a LineString
     if isinstance(ob, LineString):
         g = cast_geom(ob._geom)
         n = GEOSGeom_getCoordinateDimension_r(handle, g)
-        return <unsigned long>GEOSGeom_clone_r(handle, g), n
+        if type(ob) == LineString:
+            return <unsigned long>GEOSGeom_clone_r(handle, g), n
+        else:
+            cs = GEOSGeom_getCoordSeq_r(handle, g)
+            cs = GEOSCoordSeq_clone_r(handle, cs)
+            return <unsigned long>GEOSGeom_createLineString_r(handle, cs), n
 
     try:
         # From array protocol
@@ -162,10 +168,16 @@ def geos_linearring_from_py(ob, update_geom=None, update_ndim=0):
     cdef int i, n, m, M, sm, sn
 
     # If a LinearRing is passed in, just clone it and return
-    if isinstance(ob, LinearRing):
+    # If a LineString is passed in, clone the coord seq and return a LinearRing
+    if isinstance(ob, LineString):
         g = cast_geom(ob._geom)
         n = GEOSGeom_getCoordinateDimension_r(handle, g)
-        return <unsigned long>GEOSGeom_clone_r(handle, g), n
+        if type(ob) == LinearRing:
+            return <unsigned long>GEOSGeom_clone_r(handle, g), n
+        else:
+            cs = GEOSGeom_getCoordSeq_r(handle, g)
+            cs = GEOSCoordSeq_clone_r(handle, cs)
+            return <unsigned long>GEOSGeom_createLinearRing_r(handle, cs), n
 
     try:
         # From array protocol

shapely/geometry/base.py
@@ -115,12 +115,22 @@ def geom_to_wkb(ob):
     return lgeos.GEOSGeomToWKB_buf(c_void_p(ob._geom), pointer(size))
 
 
-def geos_geom_from_py(ob):
-    """ Helper function for geos_*_from_py functions in each geom type. """
-    geom = lgeos.GEOSGeom_clone(ob._geom)
+def geos_geom_from_py(ob, create_func=None):
+    """Helper function for geos_*_from_py functions in each geom type.
+
+    If a create_func is specified the coodinate sequence is cloned and a new
+    geometry is created with it, otherwise the geometry is cloned directly.
+    This behaviour is useful for converting between LineString and LinearRing
+    objects.
+    """
+    if create_func is None:
+        geom = lgeos.GEOSGeom_clone(ob._geom)
+    else:
+        cs = lgeos.GEOSGeom_getCoordSeq(ob._geom)
+        cs = lgeos.GEOSCoordSeq_clone(cs)
+        geom = create_func(cs)
     N = lgeos.GEOSGeom_getCoordinateDimension(geom)
     return geom, N
-
 
 def exceptNull(func):
     """Decorator which helps avoid GEOS operations on null pointers."""

shapely/geometry/linestring.py
@@ -181,8 +181,12 @@ def asLineString(context):
 
 def geos_linestring_from_py(ob, update_geom=None, update_ndim=0):
     # If a LineString is passed in, clone it and return
+    # If a LinearRing is passed in, clone the coord seq and return a LineString
     if isinstance(ob, LineString):
-        return geos_geom_from_py(ob)
+        if type(ob) == LineString:
+            return geos_geom_from_py(ob)
+        else:
+            return geos_geom_from_py(ob, lgeos.GEOSGeom_createLineString)
 
     # If numpy is present, we use numpy.require to ensure that we have a
     # C-continguous array that owns its data. View data will be copied.

commit 649fff87fc0740e9e92462feb75a79832bf8a62d

Fixed regression in 45ee253 by checking GEOSisRing first.

shapely/_geos.pxi
@@ -41,6 +41,8 @@ cdef extern from "geos_c.h":
     char GEOSPreparedTouches_r(GEOSContextHandle_t, const GEOSPreparedGeometry*, const GEOSGeometry*) nogil
     char GEOSPreparedWithin_r(GEOSContextHandle_t, const GEOSPreparedGeometry*, const GEOSGeometry*) nogil
 
+    char GEOSisRing_r(GEOSContextHandle_t, GEOSGeometry *) nogil
+
 
 cdef GEOSContextHandle_t get_geos_context_handle():
     # Note: This requires that lgeos is defined, so needs to be imported as:

shapely/geometry/polygon.py
@@ -350,7 +350,8 @@ def geos_linearring_from_py(ob, update_geom=None, update_ndim=0):
         if type(ob) == LinearRing:
             return geos_geom_from_py(ob)
         else:
-            return geos_geom_from_py(ob, lgeos.GEOSGeom_createLinearRing)
+            if ob.is_ring:
+                return geos_geom_from_py(ob, lgeos.GEOSGeom_createLinearRing)
 
     # If numpy is present, we use numpy.require to ensure that we have a
     # C-continguous array that owns its data. View data will be copied.

shapely/speedups/_speedups.pyx
@@ -175,9 +175,10 @@ def geos_linearring_from_py(ob, update_geom=None, update_ndim=0):
         if type(ob) == LinearRing:
             return <unsigned long>GEOSGeom_clone_r(handle, g), n
         else:
-            cs = GEOSGeom_getCoordSeq_r(handle, g)
-            cs = GEOSCoordSeq_clone_r(handle, cs)
-            return <unsigned long>GEOSGeom_createLinearRing_r(handle, cs), n
+            if GEOSisRing_r(handle, g):
+                cs = GEOSGeom_getCoordSeq_r(handle, g)
+                cs = GEOSCoordSeq_clone_r(handle, cs)
+                return <unsigned long>GEOSGeom_createLinearRing_r(handle, cs), n
 
     try:
         # From array protocol