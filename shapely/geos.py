b8ecce20a4e4a431cefea316ed120b7bee761693
Sean Gillies <sean@mapbox.com>
2015-02-23 21:54:39 -0700

    Smarter error handling.
    Uses c_void_p as suggested by @QuLogic and dodges the one case
    where the value might be something other than a string.

--- shapely/geos.py
+++ shapely/geos.py
@@ -8,7 +8,8 @@ import sys
 import atexit
 import logging
 import threading
-from ctypes import CDLL, cdll, pointer, c_void_p, c_size_t, c_char_p, string_at
+from ctypes import CDLL, cdll, pointer, string_at, cast, POINTER
+from ctypes import c_void_p, c_size_t, c_char_p, c_int, c_float
 from ctypes.util import find_library
 
 from . import ftools
@@ -184,18 +185,47 @@ class TopologicalError(Exception):
 class PredicateError(Exception):
     pass
 
+#def error_handler(fmt, *args):
+#    if sys.version_info[0] >= 3:
+#        fmt = fmt.decode('')
+#        args = [arg.decode('ascii') for arg in args]
+#    LOG.error(fmt, *args)
+
+# While this function can take any number of positional arguments when
+# called from Python and GEOS expects its error handler to accept any
+# number of arguments (like printf), I'm unable to get ctypes to make
+# a callback object from this function that will accept any number of
+# arguments.
+#
+# At the moment, functions in the GEOS C API only pass 0 or 1 arguments
+# to the error handler. We can deal with this, but when if that changes,
+# Shapely may break.
 
 def error_handler(fmt, *args):
     fmt = fmt.decode('ascii')
     conversions = re.findall(r'%.', fmt)
-    args = [arg.decode('ascii') for spec, arg in zip(conversions, args)]
-    LOG.error(fmt, *args)
+    log_vals = []
+    for spec, arg in zip(conversions, args):
+        if spec == '%s' and arg is not None:
+            log_vals.append(string_at(arg).decode('ascii'))
+        else:
+            LOG.error("An error occurred, but the format string "
+                      "'%s' could not be converted.", fmt)
+            return
+    LOG.error(fmt, *log_vals)
 
 
 def notice_handler(fmt, args):
     fmt = fmt.decode('ascii')
     conversions = re.findall(r'%.', fmt)
-    args = [arg.decode('ascii') for spec, arg in zip(conversions, args)]
+    log_vals = []
+    for spec, arg in zip(conversions, args):
+        if spec == '%s' and arg is not None:
+            log_vals.append(string_at(arg).decode('ascii'))
+        else:
+            LOG.error("An error occurred, but the format string "
+                      "'%s' could not be converted.", fmt)
+            return
     LOG.warning(fmt, args)
 
 error_h = EXCEPTION_HANDLER_FUNCTYPE(error_handler)
