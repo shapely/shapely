87f4e6f8a4acfe4831a67bd94dbf317c73f27c3b
Joshua Arnott <josh@snorfalorpagus.net>
2015-12-21 23:09:44 +0000

    Exclude tkinter

--- cxfreeze_example/setup.py
+++ cxfreeze_example/setup.py
@@ -1,7 +1,7 @@
 import sys
 from cx_Freeze import setup, Executable
 
-build_exe_options = {"packages": ["os", "shapely", "shapely.libgeos"], "excludes": []}
+build_exe_options = {"packages": ["os", "shapely", "shapely.libgeos"], "excludes": ["tkinter"]}
 
 setup(name="example", version="1.0",
       description="Example frozen shapely application",
