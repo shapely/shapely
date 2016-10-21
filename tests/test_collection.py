887b0817e23703e07143b9113fbb65f6aa820ff8
Thibault Deutsch <thibault.deutsch@gmail.com>
2016-10-21 15:13:43 +0200

    Add tests for geointerface of GeometryCollection

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -1,6 +1,8 @@
 from . import unittest
 from shapely.geometry import LineString
 from shapely.geometry.collection import GeometryCollection
+from shapely.geometry import shape
+from shapely.geometry import asShape
 
 
 class CollectionTestCase(unittest.TestCase):
@@ -23,6 +25,43 @@ class CollectionTestCase(unittest.TestCase):
 
         # access geometry, this should not seg fault as 1.2.15 did
         self.assertIsNotNone(child.wkt)
+
+    def test_geointerface(self):
+        d = {"type": "GeometryCollection","geometries": [
+                {"type": "Point", "coordinates": (0, 3)},
+                {"type": "LineString", "coordinates": ((2, 0), (1, 0))}
+            ]}
+
+        # asShape
+        m = asShape(d)
+        self.assertEqual(m.geom_type, "GeometryCollection")
+        self.assertEqual(len(m), 2)
+        geom_types = [g.geom_type for g in m.geoms]
+        self.assertIn("Point", geom_types)
+        self.assertIn("LineString", geom_types)
+
+        # shape
+        m = shape(d)
+        self.assertEqual(m.geom_type, "GeometryCollection")
+        self.assertEqual(len(m), 2)
+        geom_types = [g.geom_type for g in m.geoms]
+        self.assertIn("Point", geom_types)
+        self.assertIn("LineString", geom_types)
+
+    def test_empty_geointerface(self):
+        d = {"type": "GeometryCollection", "geometries": []}
+
+        # asShape
+        m = asShape(d)
+        self.assertEqual(m.geom_type, "GeometryCollection")
+        self.assertEqual(len(m), 0)
+        self.assertEqual(m.geoms, [])
+
+        # shape
+        m = shape(d)
+        self.assertEqual(m.geom_type, "GeometryCollection")
+        self.assertEqual(len(m), 0)
+        self.assertEqual(m.geoms, [])
 
 
 def test_suite():
