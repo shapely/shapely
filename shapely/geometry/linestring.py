ea53cd5a32ceb6d3675a35db7d665abb8ff25e21
Joshua Arnott <josh@snorfalorpagus.net>
2016-12-10 16:26:29 +0000

    Implemented array interface for empty Point and LineString.
    Closes #403.

--- shapely/geometry/linestring.py
+++ shapely/geometry/linestring.py
@@ -83,7 +83,11 @@ class LineString(BaseGeometry):
 
     def array_interface(self):
         """Provide the Numpy array protocol."""
-        return self.coords.array_interface()
+        if self.is_empty:
+            ai = {'version': 3, 'typestr': '<f8', 'shape': (0,), 'data': (c_double * 0)()}
+        else:
+            ai = self.coords.array_interface()
+        return ai
 
     __array_interface__ = property(array_interface)
 
