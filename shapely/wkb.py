89bd0780b9c79c2a1a1a38814287482ad1fc7ef7
Joshua Arnott <josh@snorfalorpagus.net>
2018-05-28 16:32:01 +0100

    Added srid keyword to wkb.dumps. Closes #592.

--- shapely/wkb.py
+++ shapely/wkb.py
@@ -1,7 +1,8 @@
 """Load/dump geometries using the well-known binary (WKB) format
 """
 
-from shapely import geos
+from shapely.geos import WKBReader, WKBWriter, lgeos
+from shapely.geometry.base import geom_factory
 
 # Pickle-like convenience functions
 
@@ -9,7 +10,7 @@ def loads(data, hex=False):
     """Load a geometry from a WKB byte string, or hex-encoded string if
     ``hex=True``.
     """
-    reader = geos.WKBReader(geos.lgeos)
+    reader = WKBReader(lgeos)
     if hex:
         return reader.read_hex(data)
     else:
@@ -20,12 +21,29 @@ def load(fp, hex=False):
     data = fp.read()
     return loads(data, hex=hex)
 
-def dumps(ob, hex=False, **kw):
+def dumps(ob, hex=False, srid=None, **kw):
     """Dump a WKB representation of a geometry to a byte string, or a
     hex-encoded string if ``hex=True``.
-
-    See available keyword output settings in ``shapely.geos.WKBWriter``."""
-    writer = geos.WKBWriter(geos.lgeos, **kw)
+    
+    Parameters
+    ----------
+    ob : geometry
+        The geometry to export to well-known binary (WKB) representation
+    hex : bool
+        If true, export the WKB as a hexidecimal string. The default is to
+        return a binary string/bytes object.
+    srid : int
+        Spatial reference system ID to include in the output. The default value
+        means no SRID is included.
+    **kw : kwargs
+        See available keyword output settings in ``shapely.geos.WKBWriter``."""
+    if srid is not None:
+        # clone the object and set the SRID before dumping
+        geom = lgeos.GEOSGeom_clone(ob._geom)
+        lgeos.GEOSSetSRID(geom, srid)
+        ob = geom_factory(geom)
+        kw["include_srid"] = True
+    writer = WKBWriter(lgeos, **kw)
     if hex:
         return writer.write_hex(ob)
     else:
