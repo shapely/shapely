affef05491f93851dd9975249a8a8c16a36393b5
Hannes <kannes@users.noreply.github.com>
2018-08-08 11:08:17 +0200

    let's call it a spatial index

--- shapely/strtree.py
+++ shapely/strtree.py
@@ -3,8 +3,8 @@ import ctypes
 
 class STRtree:
     """
-    STRtree is an R-tree that is created using the Sort-Tile-Recursive
-    algorithm.
+    STRtree is an R-tree spatial index that is created using the
+    Sort-Tile-Recursive algorithm.
     
     STRtree takes a sequence of geometry objects as initialization
     parameter. After initialization the query method can be used to make a
