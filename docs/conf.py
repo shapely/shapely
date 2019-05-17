4cf9221f4801c4188bad4e2a727fcad037ee561b
Sean Gillies <sean.gillies@gmail.com>
2019-05-17 16:16:32 -0600

    Revert to default title

--- docs/conf.py
+++ docs/conf.py
@@ -120,7 +120,7 @@ html_theme_path = ['themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "Shapely 1.6 documentation"
+# html_title = "Shapely 1.6 documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
