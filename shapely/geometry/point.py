b685e6b2eb13b0a3e63f92412a458760db9165b7
Joshua Arnott <josh@snorfalorpagus.net>
2019-05-28 21:08:21 +0100

    Bounds of an empty point is an empty tuple. Fixes #716

--- shapely/geometry/point.py
+++ shapely/geometry/point.py
@@ -118,7 +118,11 @@ class Point(BaseGeometry):
 
     @property
     def bounds(self):
-        xy = self.coords[0]
+        """Returns minimum bounding region (minx, miny, maxx, maxy)"""
+        try:
+            xy = self.coords[0]
+        except IndexError:
+            return ()
         return (xy[0], xy[1], xy[0], xy[1])
 
     # Coordinate access
