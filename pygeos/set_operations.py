5feb8b19d33f0ddbcba80e8068590771484f462f
Casper van der Wel <caspervdw@gmail.com>
2019-08-17 16:17:41 +0200

    Finish setop docs

--- pygeos/set_operations.py
+++ pygeos/set_operations.py
@@ -1,134 +1,197 @@
-import numpy as np
-from . import ufuncs, Empty, Geometry
+from . import ufuncs, Empty, Geometry, GeometryType
 
-__all__ = ["difference", "intersection", "symmetric_difference", "union"]
+__all__ = [
+    "difference",
+    "intersection",
+    "intersection_all",
+    "symmetric_difference",
+    "symmetric_difference_all",
+    "union",
+    "union_all",
+]
 
 
-def difference(a, b=None, **kwargs):
+def difference(a, b, **kwargs):
     """Returns the part of geometry A that does not intersect with geometry B.
 
     Parameters
     ----------
     a : Geometry or array_like
-    b : Geometry or array_like or None
-    axis : int
-        The axis in a to perform the difference on. This a.shape[axis] is
-        required to be 2. The axis is ignored when b is not None.
+    b : Geometry or array_like
 
     Examples
     --------
-    >>> line_1 = Geometry("LINESTRING(0 0, 2 2)")
-    >>> line_2 = Geometry("LINESTRING(1 1, 3 3)")
-    >>> difference(line_1, line_2)
+    >>> line = Geometry("LINESTRING (0 0, 2 2)")
+    >>> difference(line, Geometry("LINESTRING (1 1, 3 3)"))
     <pygeos.Geometry LINESTRING (0 0, 1 1)>
-    >>> difference([line_2, line_1])
-    <pygeos.Geometry LINESTRING (2 2, 3 3)>
-    >>> difference([[line_1, line_2], [line_1, line_1]], axis=1).tolist()
-    [<pygeos.Geometry LINESTRING (0 0, 1 1)>, <pygeos.Empty>]
-    >>> difference([[line_1, line_2], [line_1, line_1]], axis=0).tolist()
-    [<pygeos.Empty>, <pygeos.Geometry LINESTRING (2 2, 3 3)>]
-    >>> difference(Empty, line_1)
+    >>> difference(Empty, line)
     <pygeos.Empty>
-    >>> difference(line_1, Empty)
+    >>> difference(line, Empty)
     <pygeos.Geometry LINESTRING (0 0, 2 2)>
     """
-    if b is None:
-        axis = kwargs.get("axis", 0)
-        a = np.asarray(a)
-        if a.shape[axis] != 2:
-            raise ValueError(
-                "Can only take a difference over an axis with length 2"
-            )
-        return ufuncs.difference.reduce(a, **kwargs)
     return ufuncs.difference(a, b, **kwargs)
 
 
-def intersection(a, b=None, axis=0):
+def intersection(a, b, **kwargs):
     """Returns the geometry that is shared between input geometries.
 
     Parameters
     ----------
     a : Geometry or array_like
-    b : Geometry or array_like or None
-    axis : int
-        The axis in a to perform the operation on. This axis can be of any
-        shape: all geometries on that axis are intersected. Ignored when b is
-        not None.
+    b : Geometry or array_like
+
+    See also
+    --------
+    intersection_all
+
+    Examples
+    --------
+    >>> line = Geometry("LINESTRING(0 0, 2 2)")
+    >>> intersection(line, Geometry("LINESTRING(1 1, 3 3)"))
+    <pygeos.Geometry LINESTRING (1 1, 2 2)>
+    >>> intersection(Empty, line)
+    <pygeos.Empty>
+    """
+    return ufuncs.intersection(a, b, **kwargs)
+
+
+def intersection_all(geometries, axis=0, **kwargs):
+    """Returns the intersection of multiple geometries.
+
+    Parameters
+    ----------
+    geometries : array_like
+    axis : int, tuple or None
+        Axis or axes along which the operation is performed. The default
+        performs the operation over the first dimension of the input array.
+        axis may be negative, in which case it counts from the last to the
+        first axis. If axis is None, the operation is performed over all the
+        axes. If it is a tuple of ints, a reduction is performed on multiple
+        axes.
+
+    See also
+    --------
+    intersection
 
     Examples
     --------
     >>> line_1 = Geometry("LINESTRING(0 0, 2 2)")
     >>> line_2 = Geometry("LINESTRING(1 1, 3 3)")
-    >>> intersection(line_1, line_2)
+    >>> intersection_all([line_1, line_2])
     <pygeos.Geometry LINESTRING (1 1, 2 2)>
-    >>> intersection([line_1, line_2, line_1], axis=0)
-    <pygeos.Geometry LINESTRING (1 1, 2 2)>
-    >>> intersection([[line_1, Empty]], axis=1).tolist()
+    >>> intersection_all([[line_1, line_2, Empty]], axis=1).tolist()
     [<pygeos.Empty>]
-    >>> intersection(Empty, line_1)
-    <pygeos.Empty>
     """
-    if b is None:
-        return ufuncs.intersection.reduce(a, axis=axis)
-    else:
-        return ufuncs.intersection(a, b)
+    return ufuncs.intersection.reduce(geometries, axis=axis, **kwargs)
 
 
-def symmetric_difference(a, b=None, axis=0):
+def symmetric_difference(a, b, **kwargs):
     """Returns the geometry that represents the portions of input geometries
     that do not intersect.
 
     Parameters
     ----------
     a : Geometry or array_like
-    b : Geometry or array_like or None
-    axis : int
-        The axis in a to perform the operation on. This axis can be of any
-        shape: all geometries on that axis are intersected. Ignored when b is
-        not None.
+    b : Geometry or array_like
+
+    See also
+    --------
+    symmetric_difference_all
+
+    Examples
+    --------
+    >>> line = Geometry("LINESTRING(0 0, 2 2)")
+    >>> symmetric_difference(line, Geometry("LINESTRING(1 1, 3 3)"))
+    <pygeos.Geometry MULTILINESTRING ((0 0, 1 1), (2 2, 3 3))>
+    >>> symmetric_difference(line, Empty)
+    <pygeos.Geometry LINESTRING (0 0, 2 2)>
+    """
+    return ufuncs.symmetric_difference(a, b, **kwargs)
+
+
+def symmetric_difference_all(geometries, axis=0, **kwargs):
+    """Returns the symmetric difference of multiple geometries.
+
+    Parameters
+    ----------
+    geometries : array_like
+    axis : int, tuple or None
+        Axis or axes along which the operation is performed. The default
+        performs the operation over the first dimension of the input array.
+        axis may be negative, in which case it counts from the last to the
+        first axis. If axis is None, the operation is performed over all the
+        axes. If it is a tuple of ints, a reduction is performed on multiple
+        axes.
+
+    See also
+    --------
+    symmetric_difference
 
     Examples
     --------
     >>> line_1 = Geometry("LINESTRING(0 0, 2 2)")
     >>> line_2 = Geometry("LINESTRING(1 1, 3 3)")
-    >>> symmetric_difference(line_1, line_2)
-    <pygeos.Geometry MULTILINESTRING ((0 0, 1 1), (2 2, 3 3))>
-    >>> symmetric_difference([line_1, line_1])
-    <pygeos.Empty>
-    >>> symmetric_difference(line_1, Empty)
-    <pygeos.Geometry LINESTRING (0 0, 2 2)>
+    >>> intersection_all([line_1, line_2])
+    <pygeos.Geometry LINESTRING (1 1, 2 2)>
+    >>> intersection_all([[line_1, line_2, Empty]], axis=1).tolist()
+    [<pygeos.Empty>]
     """
-    if b is None:
-        return ufuncs.symmetric_difference.reduce(a, axis=axis)
-    else:
-        return ufuncs.symmetric_difference(a, b)
+    return ufuncs.symmetric_difference.reduce(geometries, axis=axis, **kwargs)
 
 
-def union(a, b=None, axis=0):
-    """Combines geometries into one.
+def union(a, b, **kwargs):
+    """Merges geometries into one.
 
     Parameters
     ----------
     a : Geometry or array_like
-    b : Geometry or array_like or None
-    axis : int
-        The axis in a to perform the operation on. This axis can be of any
-        shape: all geometries on that axis are intersected. Ignored when b is
-        not None.
+    b : Geometry or array_like
+
+    See also
+    --------
+    union_all
+
+    Examples
+    --------
+    >>> line = Geometry("LINESTRING(0 0, 2 2)")
+    >>> union(line, Geometry("LINESTRING(2 2, 3 3)"))
+    <pygeos.Geometry MULTILINESTRING ((0 0, 2 2), (2 2, 3 3))>
+    >>> union(line, Empty)
+    <pygeos.Geometry LINESTRING (0 0, 2 2)>
+    """
+    return ufuncs.union(a, b, **kwargs)
+
+
+def union_all(geometries, axis=0, **kwargs):
+    """Returns the union of multiple geometries.
+
+    Parameters
+    ----------
+    geometries : array_like
+    axis : int, tuple or None
+        Axis or axes along which the operation is performed. The default
+        performs the operation over the first dimension of the input array.
+        axis may be negative, in which case it counts from the last to the
+        first axis. If axis is None, the operation is performed over all the
+        axes. If it is a tuple of ints, a reduction is performed on multiple
+        axes.
+
+    See also
+    --------
+    union
 
     Examples
     --------
     >>> line_1 = Geometry("LINESTRING(0 0, 2 2)")
-    >>> line_2 = Geometry("LINESTRING(1 1, 3 3)")
-    >>> union([line_1, line_2])
-    <pygeos.Geometry LINESTRING (0 0, 3 3)>
-    >>> union([line_1, line_2, line_2])
-    <pygeos.Geometry LINESTRING (0 0, 3 3)>
-    >>> union(line_1, Empty)
-    <pygeos.Empty>
+    >>> line_2 = Geometry("LINESTRING(2 2, 3 3)")
+    >>> union_all([line_1, line_2])
+    <pygeos.Geometry MULTILINESTRING ((0 0, 2 2), (2 2, 3 3))>
+    >>> union_all([[line_1, line_2, Empty]], axis=1).tolist()
+    [<pygeos.Geometry MULTILINESTRING ((0 0, 2 2), (2 2, 3 3))>]
     """
-    if b is None:
-        a = ufuncs.create_collection(a, axis=axis)
-        return ufuncs.unary_union(a)
-    else:
-        return ufuncs.union(a, b)
+    # for union_all, GEOS provides an efficient route through first creating
+    # GeometryCollections
+    collections = ufuncs.create_collection(
+        geometries, GeometryType.GEOMETRYCOLLECTION, axis=axis
+    )
+    return ufuncs.unary_union(collections, **kwargs)
