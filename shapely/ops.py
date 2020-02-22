aa97de38e6101b32c5f91b8444fe51f8c5f78c28
Kyle Barron <barronk@mit.edu>
2020-02-21 17:53:42 -0700

    Simplify distance calculation

--- shapely/ops.py
+++ shapely/ops.py
@@ -533,7 +533,7 @@ def substring(geom, start_dist, end_dist, normalized=False):
         elif current_distance >= max_dist:
             break
 
-        current_distance += LineString([p1, p2]).length
+        current_distance += ((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2) ** 0.5
 
     if start_dist < end_dist:
         vertex_list.append((end_point.x, end_point.y))
