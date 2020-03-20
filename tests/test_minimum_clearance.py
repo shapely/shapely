a826eb821b36e2512ffa4eda03ebd6c5fc21ae23
Tom Caruso <carusot42@gmail.com>
2020-03-20 16:54:11 -0400

    fix GEOS version.

--- tests/test_minimum_clearance.py
+++ tests/test_minimum_clearance.py
@@ -10,7 +10,7 @@ from shapely.wkt import loads as load_wkt
 from shapely.geos import geos_version
 
 
-@unittest.skipIf(geos_version < (3, 9, 0),
+@unittest.skipIf(geos_version < (3, 6, 0),
                  "GEOS > 3.6.0 is required.")
 class TestMinimumClearance(unittest.TestCase):
 
