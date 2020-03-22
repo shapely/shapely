4308555f4683acaef2390fb4ee5a93c935cc0d7e
Tom Caruso <carusot42@gmail.com>
2020-03-22 00:49:47 -0400

    move geos-config version check to just before pip install

--- ci/travis/install_geos.sh
+++ ci/travis/install_geos.sh
@@ -41,7 +41,6 @@ if [ "$GEOSVERSION" = "master" ]; then
 else
     if [ -d "$GEOSINSTVERSION/include/geos" ]; then
         echo "Using cached install $GEOSINSTVERSION"
-        echo "geos-config version: $($HOME/geosinstall/geos-$GEOSVERSION/bin/geos-config --version)"
     else
         wget -q http://download.osgeo.org/geos/geos-$GEOSVERSION.tar.bz2
         tar xfj geos-$GEOSVERSION.tar.bz2
