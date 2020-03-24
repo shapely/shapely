6c66b28ea8ac18681cd39f16444f84a830e68e80
Tom Caruso <carusot42@gmail.com>
2020-03-24 18:09:04 -0400

    switch back to pytest

--- tests/test_minimum_clearance.py
+++ tests/test_minimum_clearance.py
@@ -4,27 +4,33 @@ Tests for the minimum clearance property.
 
 import math
 
-import unittest
+import pytest
 
 from shapely.wkt import loads as load_wkt
 from shapely.geos import geos_version
 
+requires_geos_36 = pytest.mark.skipif(geos_version < (3, 6, 0), "GEOS > 3.6.0 is required.")
 
-@unittest.skipIf(geos_version < (3, 6, 0), "GEOS > 3.6.0 is required.")
-class TestMinimumClearance(unittest.TestCase):
 
-    def test_point(self):
-        point = load_wkt("POINT (0 0)")
-        self.assertEqual(math.inf, point.minimum_clearance)
+@requires_geos_36
+def test_point():
+    point = load_wkt("POINT (0 0)")
+    assert point.minimum_clearance == math.inf
 
-    def test_linestring(self):
-        line = load_wkt('LINESTRING (0 0, 1 1, 2 2)')
-        self.assertEqual(1.414214, round(line.minimum_clearance, 6))
 
-    def test_simple_polygon(self):
-        poly = load_wkt('POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))')
-        self.assertEqual(1.0, poly.minimum_clearance)
+@requires_geos_36
+def test_linestring():
+    line = load_wkt('LINESTRING (0 0, 1 1, 2 2)')
+    assert round(line.minimum_clearance, 6) == 1.414214
 
-    def test_more_complicated_polygon(self):
-        poly = load_wkt('POLYGON ((20 20, 34 124, 70 140, 130 130, 70 100, 110 70, 170 20, 90 10, 20 20))')
-        self.assertEqual(35.777088, round(poly.minimum_clearance, 6))
+
+@requires_geos_36
+def test_simple_polygon():
+    poly = load_wkt('POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))')
+    assert poly.minimum_clearance == 1.0
+
+
+@requires_geos_36
+def test_more_complicated_polygon():
+    poly = load_wkt('POLYGON ((20 20, 34 124, 70 140, 130 130, 70 100, 110 70, 170 20, 90 10, 20 20))')
+    assert round(poly.minimum_clearance, 6) == 35.777088
