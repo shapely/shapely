8d5a7e2a89ff142669161fd8a5662d60fe3de09d
Mike Taves <mwtoews@gmail.com>
2020-03-31 22:05:44 +1300

    Shift AppVeyor setup to move away from wheel-building to just testing (#864)

--- ci/appveyor/build_geos.cmd
+++ ci/appveyor/build_geos.cmd
@@ -10,17 +10,16 @@ if exist %GEOSINSTALL% (
   curl -fsSO http://download.osgeo.org/geos/geos-%GEOSVERSION%.tar.bz2
   7z x geos-%GEOSVERSION%.tar.bz2
   7z x geos-%GEOSVERSION%.tar
-  cd geos-%GEOSVERSION%
+  cd geos-%GEOSVERSION% || exit /B 5
 
   pip install ninja
   cmake --version
 
   mkdir build
   cd build
-  cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%GEOSINSTALL% ..
-  cmake --build . --config Release -j 2
-  ctest . --config Release
-  cmake --install . --config Release
+  cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%GEOSINSTALL% .. || exit /B 1
+  cmake --build . --config Release || exit /B 2
+  ctest . --config Release || exit /B 3
+  cmake --install . --config Release || exit /B 4
   cd ..
 )
-
