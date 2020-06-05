6b344455dfb186669e65d99a34177d34e3e1a88d
bhavika <bhavicka.7992@gmail.com>
2020-06-05 13:25:27 -0400

    Add test

--- tests/test_predicates.py
+++ tests/test_predicates.py
@@ -20,6 +20,7 @@ class PredicatesTestCase(unittest.TestCase):
         self.assertFalse(point.touches(Point(-1.0, -1.0)))
         self.assertTrue(point.equals(Point(0.0, 0.0)))
         self.assertTrue(point.covers(Point(0.0, 0.0)))
+        self.assertTrue(point.covered_by(Point(0.0, 0.0)))
         self.assertFalse(point.covers(Point(-1.0, -1.0)))
 
     def test_unary_predicates(self):
