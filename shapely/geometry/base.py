8329921fadf5255fbb48c666285b0f145e7d3542
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-08-26 22:15:45 +0200

    [2.0] Remove mutability of geometries  (#960)
    * keep tests for immutability

--- shapely/geometry/base.py
+++ shapely/geometry/base.py
@@ -316,13 +316,9 @@ class BaseGeometry:
 
     def _get_coords(self):
         """Access to geometry's coordinates (CoordinateSequence)"""
-        raise NotImplementedError("set_coords must be provided by derived classes")
+        return CoordinateSequence(self)
 
-    def _set_coords(self, ob):
-        raise NotImplementedError(
-            "set_coords must be provided by derived classes")
-
-    coords = property(_get_coords, _set_coords)
+    coords = property(_get_coords)
 
     @property
     def xy(self):
@@ -853,10 +849,6 @@ class BaseMultipartGeometry(BaseGeometry):
                                   "provide the array interface")
 
     def _get_coords(self):
-        raise NotImplementedError("Sub-geometries may have coordinate "
-                                  "sequences, but collections do not")
-
-    def _set_coords(self, ob):
         raise NotImplementedError("Sub-geometries may have coordinate "
                                   "sequences, but collections do not")
 
--- shapely/geometry/polygon.py
+++ shapely/geometry/polygon.py
@@ -64,26 +64,6 @@ class LinearRing(LineString):
             'coordinates': tuple(self.coords)
             }
 
-    # Coordinate access
-
-    def _get_coords(self):
-        """Access to geometry's coordinates (CoordinateSequence)"""
-        return CoordinateSequence(self)
-
-    def _set_coords(self, coordinates):
-        warnings.warn(
-            "Setting the 'coords' to mutate a Geometry in place is deprecated,"
-            " and will not be possible any more in Shapely 2.0",
-            ShapelyDeprecationWarning, stacklevel=2)
-        self._empty()
-        ret = geos_linearring_from_py(coordinates)
-        if ret is not None:
-            geom, n = ret
-            self._set_geom(geom)
-            self._ndim = n
-
-    coords = property(_get_coords, _set_coords)
-
     def __setstate__(self, state):
         """WKB doesn't differentiate between LineString and LinearRing so we
         need to move the coordinate sequence into the correct geometry type"""
@@ -321,10 +301,6 @@ class Polygon(BaseGeometry):
         "A polygon does not itself provide the array interface. Its rings do.")
 
     def _get_coords(self):
-        raise NotImplementedError(
-        "Component rings have coordinate sequences, but the polygon does not")
-
-    def _set_coords(self, ob):
         raise NotImplementedError(
         "Component rings have coordinate sequences, but the polygon does not")
 
