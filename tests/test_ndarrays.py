4ea50c98023dc386b072a0bb3c2160747775a06e
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-08-28 16:43:49 +0200

    [2.0] Remove deprecated asShape / adapter classes (#961)

--- tests/test_ndarrays.py
+++ tests/test_ndarrays.py
@@ -15,40 +15,6 @@ except ImportError:
 
 class TransposeTestCase(unittest.TestCase):
 
-    @shapely20_deprecated
-    @unittest.skipIf(not numpy, 'numpy not installed')
-    def test_multipoint_adapter(self):
-        arr = numpy.array([[1.0, 1.0, 2.0, 2.0, 1.0], [3.0, 4.0, 4.0, 3.0, 3.0]])
-        tarr = arr.T
-        shape = geometry.asMultiPoint(tarr)
-        coords = reduce(lambda x, y: x + y, [list(g.coords) for g in shape.geoms])
-        self.assertEqual(
-            coords,
-            [(1.0, 3.0), (1.0, 4.0), (2.0, 4.0), (2.0, 3.0), (1.0, 3.0)]
-        )
-
-    @shapely20_deprecated
-    @unittest.skipIf(not numpy, 'numpy not installed')
-    def test_linestring_adapter(self):
-        a = numpy.array([[1.0, 1.0, 2.0, 2.0, 1.0], [3.0, 4.0, 4.0, 3.0, 3.0]])
-        t = a.T
-        s = geometry.asLineString(t)
-        self.assertEqual(
-            list(s.coords),
-            [(1.0, 3.0), (1.0, 4.0), (2.0, 4.0), (2.0, 3.0), (1.0, 3.0)]
-        )
-
-    @shapely20_deprecated
-    @unittest.skipIf(not numpy, 'numpy not installed')
-    def test_polygon_adapter(self):
-        a = numpy.array([[1.0, 1.0, 2.0, 2.0, 1.0], [3.0, 4.0, 4.0, 3.0, 3.0]])
-        t = a.T
-        s = geometry.asPolygon(t)
-        self.assertEqual(
-            list(s.exterior.coords),
-            [(1.0, 3.0), (1.0, 4.0), (2.0, 4.0), (2.0, 3.0), (1.0, 3.0)]
-        )
-
     @unittest.skipIf(not numpy, 'numpy not installed')
     def test_multipoint(self):
         arr = numpy.array([[1.0, 1.0, 2.0, 2.0, 1.0], [3.0, 4.0, 4.0, 3.0, 3.0]])
