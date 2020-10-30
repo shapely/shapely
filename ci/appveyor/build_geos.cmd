c2a838bb684ab569fe969a76b987835d8bf89c13
Mike Taves <mwtoews@gmail.com>
2020-10-30 14:13:20 +1300

    Add Python 3.9 support to setup.py and CI
    * Change AppVeyor image from 2017 to Visual Studio 2019
    * Rename environment variable GEOSVERSION -> GEOS_VERSION
    * Rename environment variable GEOSINSTALL -> GEOS_INSTALL
    * Rename other local environment variables

--- ci/appveyor/build_geos.cmd
+++ ci/appveyor/build_geos.cmd
@@ -1,23 +1,23 @@
 REM This script is called from appveyor.yml
 
-if exist %GEOSINSTALL% (
-  echo Using cached %GEOSINSTALL%
+if exist %GEOS_INSTALL% (
+  echo Using cached %GEOS_INSTALL%
 ) else (
-  echo Building %GEOSINSTALL%
+  echo Building %GEOS_INSTALL%
 
   cd C:\projects
 
-  curl -fsSO http://download.osgeo.org/geos/geos-%GEOSVERSION%.tar.bz2
-  7z x geos-%GEOSVERSION%.tar.bz2
-  7z x geos-%GEOSVERSION%.tar
-  cd geos-%GEOSVERSION% || exit /B 5
+  curl -fsSO http://download.osgeo.org/geos/geos-%GEOS_VERSION%.tar.bz2
+  7z x geos-%GEOS_VERSION%.tar.bz2
+  7z x geos-%GEOS_VERSION%.tar
+  cd geos-%GEOS_VERSION% || exit /B 5
 
   pip install ninja
   cmake --version
 
   mkdir build
   cd build
-  cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%GEOSINSTALL% .. || exit /B 1
+  cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%GEOS_INSTALL% .. || exit /B 1
   cmake --build . --config Release || exit /B 2
   ctest . --config Release || exit /B 3
   cmake --install . --config Release || exit /B 4
