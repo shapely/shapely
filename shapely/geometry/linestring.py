4ee67c5b7901fd4a8335fb493a4a514e665e1311
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-11-24 13:49:51 +0100

    remove impl in linestring parallel_offset

--- shapely/geometry/linestring.py
+++ shapely/geometry/linestring.py
@@ -140,11 +140,11 @@ class LineString(BaseGeometry):
         if mitre_limit == 0.0:
             raise ValueError(
                 'Cannot compute offset from zero-length line segment')
-        try:
-            return geom_factory(self.impl['parallel_offset'](
-                self, distance, resolution, join_style, mitre_limit, side))
-        except OSError:
-            raise TopologicalError()
+        if side == 'right':
+            distance *= -1
+        return pygeos.offset_curve(
+            self, distance, resolution, join_style, mitre_limit
+        )
 
 
 pygeos.lib.registry[1] = LineString
