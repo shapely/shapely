91d895d1e30e045b0154d3c744a9979c5fb9cccd
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-11-27 11:41:20 +0100

    Remove cython code: remove the speedups module + refactor the vectorized module (#1036)

--- shapely/vectorized/__init__.py
+++ shapely/vectorized/__init__.py
@@ -1,3 +1,69 @@
 """Provides multi-point element-wise operations such as ``contains``."""
 
-from ._vectorized import (contains, touches)
+import numpy as np
+import pygeos
+
+
+def _construct_points(x, y):
+    x, y = np.asanyarray(x), np.asanyarray(y)
+    if x.shape != y.shape:
+        raise ValueError('X and Y shapes must be equivalent.')
+
+    if x.dtype != np.float64:
+        x = x.astype(np.float64)
+    if y.dtype != np.float64:
+        y = y.astype(np.float64)
+
+    return pygeos.points(x, y)
+
+
+def contains(geometry, x, y):
+    """
+    Vectorized (element-wise) version of `contains` which checks whether
+    multiple points are contained by a single geometry.
+
+    Parameters
+    ----------
+    geometry : PreparedGeometry or subclass of BaseGeometry
+        The geometry which is to be checked to see whether each point is
+        contained within. The geometry will be "prepared" if it is not already
+        a PreparedGeometry instance.
+    x : array
+        The x coordinates of the points to check.
+    y : array
+        The y coordinates of the points to check.
+
+    Returns
+    -------
+    Mask of points contained by the given `geometry`.
+
+    """
+    points = _construct_points(x, y)
+    pygeos.prepare(geometry)
+    return pygeos.contains(geometry, points)
+
+
+def touches(geometry, x, y):
+    """
+    Vectorized (element-wise) version of `touches` which checks whether
+    multiple points touch the exterior of a single geometry.
+
+    Parameters
+    ----------
+    geometry : PreparedGeometry or subclass of BaseGeometry
+        The geometry which is to be checked to see whether each point is
+        contained within. The geometry will be "prepared" if it is not already
+        a PreparedGeometry instance.
+    x : array
+        The x coordinates of the points to check.
+    y : array
+        The y coordinates of the points to check.
+
+    Returns
+    -------
+    Mask of points which touch the exterior of the given `geometry`.
+
+    """
+    points = _construct_points(x, y)
+    pygeos.prepare(geometry)
+    return pygeos.touches(geometry, points)
