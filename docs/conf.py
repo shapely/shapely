a033ff59b0e2f4cbb31d8a86b0cd7c1fb7c03b37
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-01-22 14:21:57 +0100

    DOC: fix style issue with numpydoc parameter names and sphinx_rtd_theme (pygeos/pygeos#283)

--- docs/conf.py
+++ docs/conf.py
@@ -50,3 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
