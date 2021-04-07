6db06e9fef5a3c7970245e3b20ff673f2e9f3e2d
Felix Divo <felix.divo@gmail.com>
2021-04-07 12:50:56 +0200

    simplify WKT tests

--- tests/test_wkt.py
+++ tests/test_wkt.py
@@ -9,13 +9,8 @@ from shapely.wkt import dumps, dump, load, loads
 
 
 @pytest.fixture(scope="module")
-def pipi():
+def some_point():
     return Point((pi, -pi))
-
-
-@pytest.fixture(scope="module")
-def pipi4():
-    return Point((pi*4, -pi*4))
 
 
 @pytest.fixture(scope="module")
@@ -23,29 +18,24 @@ def null_geometry():
     return Point()
 
 
-def test_wkt_simple(pipi):
+def test_wkt(some_point):
     """.wkt and wkt.dumps() both do not trim by default."""
-    assert pipi.wkt == "POINT ({0:.15f} {1:.15f})".format(pi, -pi)
-
-
-def test_wkt(pipi4):
-    """.wkt and wkt.dumps() both do not trim by default."""
-    assert pipi4.wkt == "POINT ({0:.14f} {1:.14f})".format(pi*4, -pi*4)
+    assert some_point.wkt == "POINT ({0:.15f} {1:.15f})".format(pi, -pi)
 
 
 def test_wkt_null(null_geometry):
     assert null_geometry.wkt == "GEOMETRYCOLLECTION EMPTY"
 
 
-def test_dump_load(pipi4):
+def test_dump_load(some_point):
     with TemporaryDirectory() as directory_path:
         file = join(directory_path, "test.wkt")
         with open(file, "w") as file_pointer:
-            dump(pipi4, file_pointer)
+            dump(some_point, file_pointer)
         with open(file, "r") as file_pointer:
             restored = load(file_pointer)
 
-    assert pipi4 == restored
+    assert some_point == restored
 
 
 def test_dump_load_null_geometry(null_geometry):
@@ -60,9 +50,9 @@ def test_dump_load_null_geometry(null_geometry):
     assert null_geometry.equals(restored)
 
 
-def test_dumps_loads(pipi4):
-    assert dumps(pipi4) == "POINT ({0:.16f} {1:.16f})".format(pi*4, -pi*4)
-    assert loads(dumps(pipi4)) == pipi4
+def test_dumps_loads(some_point):
+    assert dumps(some_point) == "POINT ({0:.16f} {1:.16f})".format(pi, -pi)
+    assert loads(dumps(some_point)) == some_point
 
 
 def test_dumps_loads_null_geometry(null_geometry):
@@ -71,5 +61,6 @@ def test_dumps_loads_null_geometry(null_geometry):
     assert loads(dumps(null_geometry)).equals(null_geometry)
 
 
-def test_dumps_precision(pipi4):
-    assert dumps(pipi4, rounding_precision=4) == "POINT ({0:.4f} {1:.4f})".format(pi*4, -pi*4)
+def test_dumps_precision(some_point):
+    assert dumps(some_point, rounding_precision=4) == \
+           "POINT ({0:.4f} {1:.4f})".format(pi, -pi)
