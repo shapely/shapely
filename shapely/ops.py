5432faa7fd0afa9d7aa137d8198e93d540e0bb1a
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-05-27 09:30:38 +0200

    nearest_points

--- shapely/ops.py
+++ shapely/ops.py
@@ -283,22 +283,15 @@ def nearest_points(g1, g2):
 
     The points are returned in the same order as the input geometries.
     """
-    seq = lgeos.methods['nearest_points'](g1._geom, g2._geom)
+    seq = pygeos.shortest_line(g1, g2)
     if seq is None:
         if g1.is_empty:
             raise ValueError('The first input geometry is empty')
         else:
             raise ValueError('The second input geometry is empty')
-    x1 = c_double()
-    y1 = c_double()
-    x2 = c_double()
-    y2 = c_double()
-    lgeos.GEOSCoordSeq_getX(seq, 0, byref(x1))
-    lgeos.GEOSCoordSeq_getY(seq, 0, byref(y1))
-    lgeos.GEOSCoordSeq_getX(seq, 1, byref(x2))
-    lgeos.GEOSCoordSeq_getY(seq, 1, byref(y2))
-    p1 = Point(x1.value, y1.value)
-    p2 = Point(x2.value, y2.value)
+
+    p1 = pygeos.get_point(seq, 0)
+    p2 = pygeos.get_point(seq, 1)
     return (p1, p2)
 
 
