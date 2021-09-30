1536da4384123d30e91e275ac6840417cdefd77e
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-09-30 12:33:40 +0200

    add speedups import to vectorized

--- shapely/vectorized/__init__.py
+++ shapely/vectorized/__init__.py
@@ -1,3 +1,6 @@
 """Provides multi-point element-wise operations such as ``contains``."""
 
+# workaround DLL findings issues on Windows (https://github.com/Toblerity/Shapely/issues/1184)
+from . import speedups
+
 from ._vectorized import (contains, touches)
