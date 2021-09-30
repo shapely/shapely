e6cc0406df3da7de0775216154f60b74af0e0e75
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-09-30 12:39:30 +0200

    put exception in try except

--- shapely/vectorized/__init__.py
+++ shapely/vectorized/__init__.py
@@ -1,6 +1,9 @@
 """Provides multi-point element-wise operations such as ``contains``."""
 
 # workaround DLL findings issues on Windows (https://github.com/Toblerity/Shapely/issues/1184)
-from . import speedups
+try:
+    from . import speedups
+except Exception:
+    pass
 
 from ._vectorized import (contains, touches)
