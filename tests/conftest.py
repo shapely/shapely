868f864904861e100f3211da84c4049c6f3c7577
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-10-28 22:23:37 +0200

    test os.add_dll_directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,14 @@
+import os
 import sys
 
 import pytest
+
+
+if os.name == 'nt' and sys.version_info[1] >= 8:
+    geos_path = os.environ.get("GEOS_INSTALL")
+    if geos_path:
+        os.add_dll_directory(geos_path + r"\bin")
+
 
 from shapely.geos import geos_version
 
