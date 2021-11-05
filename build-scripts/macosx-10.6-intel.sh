8b9b7d33c5e6170aa4e8cfcf1ac2df0b4308854e
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-11-05 09:23:12 +0100

    Clean-up old/unused files and scripts

--- build-scripts/macosx-10.6-intel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Dependent on the Kyngchaos Frameworks:
-# http://www.kyngchaos.com/software/frameworks
-
-export GEOS_CONFIG="/Library/Frameworks/GEOS.framework/Versions/3/unix/bin/geos-config"
-CFLAGS="`$GEOS_CONFIG --cflags`" LDFLAGS="`$GEOS_CONFIG --clibs`" python setup.py bdist_wheel
-delocate-wheel -w fixed_wheels --require-archs=intel -v dist/Shapely-1.5.2-cp27-none-macosx_10_6_intel.whl
