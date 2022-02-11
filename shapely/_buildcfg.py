57d63566427dbf098eb123dca6d4c5d8fb969b74
Sean Gillies <sean.gillies@gmail.com>
2022-02-11 09:13:14 -0700

    Enable setup logging

--- shapely/_buildcfg.py
+++ shapely/_buildcfg.py
@@ -111,6 +111,7 @@ if not lgeos and geos_config:
             break
         except Exception as exc:
             log.info("CDLL failed", exc_info=exc)
+            print("CDLL failed: exc_info={}".format(exc))
             guesses.append(geos_library_path)
     if lgeos:
         if hasattr(lgeos, 'GEOSversion'):
