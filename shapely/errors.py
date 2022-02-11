d2da26aea0a2d49ea37acca4e9edc111f686aabe
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-02-11 22:16:09 +0100

    CLN: remove no longer used custom error classes (#1306)

--- shapely/errors.py
+++ shapely/errors.py
@@ -10,28 +10,12 @@ class UnsupportedGEOSVersionError(ShapelyError):
     """Raised when the system's GEOS library version is unsupported."""
 
 
-class ReadingError(ShapelyError):
-    """A WKT or WKB reading error."""
-
-
-class WKBReadingError(ReadingError):
-    """A WKB reading error."""
-
-
-class WKTReadingError(ReadingError):
-    """A WKT reading error."""
-
-
 class DimensionError(ShapelyError):
     """An error in the number of coordinate dimensions."""
 
 
 class TopologicalError(ShapelyError):
     """A geometry is invalid or topologically incorrect."""
-
-
-class PredicateError(ShapelyError):
-    """A geometric predicate has failed to return True/False."""
 
 
 class ShapelyDeprecationWarning(FutureWarning):
@@ -54,20 +38,6 @@ class GeometryTypeError(ShapelyError, TypeError, ValueError):
     def __init__(self, msg):
         warnings.warn(
             "GeometryTypeError will derive from ShapelyError and not TypeError or ValueError in Shapely 2.0.",
-            ShapelyDeprecationWarning,
-            stacklevel=2,
-        )
-        super().__init__(msg)
-
-
-class InvalidGeometryError(ShapelyError, TypeError, ValueError):
-    """
-    An error raised when an operation is attempted on a null geometry
-    """
-
-    def __init__(self, msg):
-        warnings.warn(
-            "InvalidGeometryError will derive from ShapelyError and not TypeError or ValueError in Shapely 2.0.",
             ShapelyDeprecationWarning,
             stacklevel=2,
         )
