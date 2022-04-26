5e39a07be1650ca94c0ada945fd822a64abafc2b
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-04-26 21:50:08 +0200

    silence more numpy warnings that happen with GEOS main

--- tests/test_prepared.py
+++ tests/test_prepared.py
@@ -45,13 +45,12 @@ def test_prepared_predicates():
     for geom2 in (polygon2, point2, polygon_empty):
         with np.errstate(invalid="ignore"):
             assert polygon1.disjoint(geom2) == prepared_polygon1.disjoint(geom2)
-        with np.errstate(invalid="ignore"):
             assert polygon1.touches(geom2) == prepared_polygon1.touches(geom2)
-        assert polygon1.intersects(geom2) == prepared_polygon1.intersects(geom2)
-        assert polygon1.crosses(geom2) == prepared_polygon1.crosses(geom2)
-        assert polygon1.within(geom2) == prepared_polygon1.within(geom2)
-        assert polygon1.contains(geom2) == prepared_polygon1.contains(geom2)
-        assert polygon1.overlaps(geom2) == prepared_polygon1.overlaps(geom2)
+            assert polygon1.intersects(geom2) == prepared_polygon1.intersects(geom2)
+            assert polygon1.crosses(geom2) == prepared_polygon1.crosses(geom2)
+            assert polygon1.within(geom2) == prepared_polygon1.within(geom2)
+            assert polygon1.contains(geom2) == prepared_polygon1.contains(geom2)
+            assert polygon1.overlaps(geom2) == prepared_polygon1.overlaps(geom2)
 
 
 def test_prepare_already_prepared():
