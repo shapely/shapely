dc8e1683703b5da3be6a2463f3e47b90b2bb484c
Enrico Ferreguti <enricofer@gmail.com>
2022-08-27 11:36:57 +0200

    test assert rewrite

--- tests/test_delaunay.py
+++ tests/test_delaunay.py
@@ -3,9 +3,12 @@ import unittest
 from shapely.geometry import Polygon, LineString, Point
 from shapely.ops import triangulate
 from shapely.geos import geos_version
+import pytest
 
-@unittest.skipIf(geos_version < (3, 4, 0), 
-                 "Delaunay triangulation not supported")
+
+pytestmark = pytest.mark.skipif(geos_version < (3, 4, 0), 
+                 reason='Delaunay triangulation not supported')
+
 class DelaunayTriangulation(unittest.TestCase):
     """
     Only testing the number of triangles and their type here.
@@ -17,17 +20,17 @@ class DelaunayTriangulation(unittest.TestCase):
 
     def test_polys(self):
         polys = triangulate(self.p)
-        self.assertEqual(len(polys), 2)
+        assert len(polys) == 2
         for p in polys:
-            self.assertTrue(isinstance(p, Polygon))
+            assert isinstance(p, Polygon)
 
     def test_lines(self):
         polys = triangulate(self.p, edges=True)
-        self.assertEqual(len(polys), 5)
+        assert len(polys) == 5
         for p in polys:
-            self.assertTrue(isinstance(p, LineString))
+            assert isinstance(p, LineString)
 
     def test_point(self):
         p = Point(1,1)
         polys = triangulate(p)
-        self.assertEqual(len(polys), 0)
+        assert len(polys) == 0
--- tests/test_geointerface.py
+++ tests/test_geointerface.py
@@ -22,21 +22,21 @@ class GeoInterfaceTestCase(unittest.TestCase):
         # Convert a dictionary
         d = {"type": "Point", "coordinates": (0.0, 0.0)}
         geom = shape(d)
-        self.assertEqual(geom.geom_type, 'Point')
-        self.assertEqual(tuple(geom.coords), ((0.0, 0.0),))
+        assert geom.geom_type, 'Point'
+        assert tuple(geom.coords) == ((0.0, 0.0),)
 
         # Convert an object that implements the geo protocol
         geom = None
         thing = GeoThing({"type": "Point", "coordinates": (0.0, 0.0)})
         geom = shape(thing)
-        self.assertEqual(geom.geom_type, 'Point')
-        self.assertEqual(tuple(geom.coords), ((0.0, 0.0),))
+        assert geom.geom_type, 'Point'
+        assert tuple(geom.coords) == ((0.0, 0.0),)
 
         # Check line string
         geom = shape(
             {'type': 'LineString', 'coordinates': ((-1.0, -1.0), (1.0, 1.0))})
-        self.assertIsInstance(geom, LineString)
-        self.assertEqual(tuple(geom.coords), ((-1.0, -1.0), (1.0, 1.0)))
+        assert isinstance(geom, LineString)
+        assert tuple(geom.coords) == ((-1.0, -1.0), (1.0, 1.0))
 
         # Check linearring
         geom = shape(
@@ -44,10 +44,10 @@ class GeoInterfaceTestCase(unittest.TestCase):
              'coordinates': 
                  ((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (2.0, -1.0), (0.0, 0.0))}
         )
-        self.assertIsInstance(geom, LinearRing)
-        self.assertEqual(
-            tuple(geom.coords),
-            ((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (2.0, -1.0), (0.0, 0.0)))
+        assert isinstance(geom, LinearRing)
+        assert tuple(geom.coords) == (
+            (0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (2.0, -1.0), (0.0, 0.0)
+        )
 
         # polygon
         geom = shape(
@@ -56,23 +56,23 @@ class GeoInterfaceTestCase(unittest.TestCase):
                 (((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (2.0, -1.0), (0.0, 0.0)),
                  ((0.1, 0.1), (0.1, 0.2), (0.2, 0.2), (0.2, 0.1), (0.1, 0.1)))}
         )
-        self.assertIsInstance(geom, Polygon)
-        self.assertEqual(
-            tuple(geom.exterior.coords),
-            ((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (2.0, -1.0), (0.0, 0.0)))
-        self.assertEqual(len(geom.interiors), 1)
+        assert isinstance(geom, Polygon)
+        assert tuple(geom.exterior.coords) == (
+            (0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (2.0, -1.0), (0.0, 0.0)
+        )
+        assert len(geom.interiors) == 1
 
         # multi point
         geom = shape({'type': 'MultiPoint',
                        'coordinates': ((1.0, 2.0), (3.0, 4.0))})
-        self.assertIsInstance(geom, MultiPoint)
-        self.assertEqual(len(geom.geoms), 2)
+        assert isinstance(geom, MultiPoint)
+        assert len(geom.geoms) == 2
 
         # multi line string
         geom = shape({'type': 'MultiLineString',
                        'coordinates': (((0.0, 0.0), (1.0, 2.0)),)})
-        self.assertIsInstance(geom, MultiLineString)
-        self.assertEqual(len(geom.geoms), 1)
+        assert isinstance(geom, MultiLineString)
+        assert len(geom.geoms) == 1
 
         # multi polygon
         geom = shape(
@@ -81,8 +81,8 @@ class GeoInterfaceTestCase(unittest.TestCase):
                 [(((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)),
                   ((0.1, 0.1), (0.1, 0.2), (0.2, 0.2), (0.2, 0.1), (0.1, 0.1))
                   )]})
-        self.assertIsInstance(geom, MultiPolygon)
-        self.assertEqual(len(geom.geoms), 1)
+        assert isinstance(geom, MultiPolygon)
+        assert len(geom.geoms) == 1
 
 
 def test_empty_wkt_polygon():
