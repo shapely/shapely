bf9020fab073ebfa7c19e0f2b99e1cc15696215b
enrico ferreguti <enricofer@gmail.com>
2022-08-27 15:58:58 +0200

    TST: rewrite tests using pytest assert (#1505)

--- tests/test_invalid_geometries.py
+++ tests/test_invalid_geometries.py
@@ -6,26 +6,22 @@ from shapely.geometry import Polygon
 from shapely.errors import TopologicalError
 
 import shapely
+import pytest
 
 
 class InvalidGeometriesTestCase(unittest.TestCase):
     def test_invalid_intersection(self):
         # Make a self-intersecting polygon
         polygon_invalid = Polygon([(0, 0), (1, 1), (1, -1), (0, 1), (0, 0)])
-        self.assertFalse(polygon_invalid.is_valid)
+        assert not polygon_invalid.is_valid
 
         # Intersect with a valid polygon
         polygon = Polygon([(-0.5, -0.5), (-0.5, 0.5), (0.5, 0.5), (0.5, -5)])
-        self.assertTrue(polygon.is_valid)
-        self.assertTrue(polygon_invalid.intersects(polygon))
-        self.assertRaises(
-            (TopologicalError, shapely.GEOSException),
-            polygon_invalid.intersection,
-            polygon,
-        )
-        self.assertRaises(
-            (TopologicalError, shapely.GEOSException),
-            polygon.intersection,
-            polygon_invalid,
-        )
+        assert polygon.is_valid
+        assert polygon_invalid.intersects(polygon)
+
+        with pytest.raises((TopologicalError, shapely.GEOSException)):
+            polygon_invalid.intersection(polygon)
+        with pytest.raises((TopologicalError, shapely.GEOSException)):
+            polygon.intersection(polygon_invalid)
         return
