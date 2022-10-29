1ca0652fa992467d3d6c2e76e47c732f6348456e
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-10-29 09:01:14 +0200

    simplify

--- shapely/geometry/point.py
+++ shapely/geometry/point.py
@@ -65,19 +65,14 @@ class Point(BaseGeometry):
             if not hasattr(coords, "__getitem__"):  # generators
                 coords = list(coords)
 
-            if isinstance(coords[0], tuple):
-                coords = coords[0]
-
-            geom = shapely.points(coords)
+            coords = np.asarray(coords).squeeze()
         else:
             # 2 or 3 args
-            geom = shapely.points(np.array(args).T)
+            coords = np.array(args).squeeze()
 
+        geom = shapely.points(coords)
         if not isinstance(geom, Point):
-            if isinstance(geom, np.ndarray) and geom.size == 1:
-                geom = geom.item()
-            else:
-                raise ValueError("Invalid values passed to Point constructor")
+            raise ValueError("Invalid values passed to Point constructor")
         return geom
 
     # Coordinate getters and setters
--- shapely/tests/geometry/test_point.py
+++ shapely/tests/geometry/test_point.py
@@ -34,6 +34,8 @@ def test_from_sequence():
     # From coordinate sequence
     p = Point([(3.0, 4.0)])
     assert p.coords[:] == [(3.0, 4.0)]
+    p = Point([[3.0, 4.0]])
+    assert p.coords[:] == [(3.0, 4.0)]
 
     # 3D
     p = Point((3.0, 4.0, 5.0))
@@ -59,6 +61,9 @@ def test_from_numpy_xy():
     # (https://github.com/shapely/shapely/issues/1587)
     p = Point(np.array([1.0]), np.array([2.0]))
     assert p.coords[:] == [(1.0, 2.0)]
+
+    p = Point(np.array([1.0]), np.array([2.0]), np.array([3.0]))
+    assert p.coords[:] == [(1.0, 2.0, 3.0)]
 
 
 def test_from_point():
