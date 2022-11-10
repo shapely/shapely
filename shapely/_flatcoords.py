2e2ef4169a64df21a96e626319240392c31e223a
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-11-10 22:48:11 +0100

    rename + handle empty geometries + add tests

--- shapely/_flatcoords.py
+++ shapely/_flatcoords.py
@@ -3,8 +3,9 @@ import numpy as np
 from . import creation
 from ._geometry import GeometryType, get_parts, get_rings, get_type_id
 from .coordinates import get_coordinates
+from .predicates import is_empty
 
-__all__ = ["to_coordinates_offsets", "from_coordinates_offsets"]
+__all__ = ["to_ragged_array", "from_ragged_array"]
 
 # # GEOS -> coords/offset arrays
 
@@ -13,14 +14,34 @@ def _get_arrays_point(arr):
     # only one array of coordinates
     coords = get_coordinates(arr)
 
+    # empty points are represented by NaNs
+    empties = is_empty(arr)
+    if empties.any():
+        indices = np.nonzero(empties)[0]
+        indices = indices - np.arange(len(indices))
+        coords = np.insert(coords, indices, np.nan, axis=0)
+
     return coords.ravel(), ()
+
+
+def _indices_to_offsets(indices, n):
+    offsets = np.insert(np.bincount(indices).cumsum(), 0, 0)
+    if len(offsets) != n + 1:
+        # last geometries might be empty or missing
+        offsets = np.pad(
+            offsets,
+            (0, n + 1 - len(offsets)),
+            "constant",
+            constant_values=offsets[-1],
+        )
+    return offsets
 
 
 def _get_arrays_multipoint(arr):
     # explode/flatten the MultiPoints
     _, part_indices = get_parts(arr, return_index=True)
     # the offsets into the multipoint parts
-    offsets = np.insert(np.bincount(part_indices).cumsum(), 0, 0)
+    offsets = _indices_to_offsets(part_indices, len(arr))
 
     # only one array of coordinates
     coords = get_coordinates(arr)
@@ -31,7 +52,7 @@ def _get_arrays_multipoint(arr):
 def _get_arrays_linestring(arr):
     # the coords and offsets into the coordinates of the linestrings
     coords, indices = get_coordinates(arr, return_index=True)
-    offsets = np.insert(np.bincount(indices).cumsum(), 0, 0)
+    offsets = _indices_to_offsets(indices, len(arr))
 
     return coords.ravel(), (offsets,)
 
@@ -40,7 +61,7 @@ def _get_arrays_multilinestring(arr):
     # explode/flatten the MultiLineStrings
     arr_flat, part_indices = get_parts(arr, return_index=True)
     # the offsets into the multilinestring parts
-    offsets2 = np.insert(np.bincount(part_indices).cumsum(), 0, 0)
+    offsets2 = _indices_to_offsets(part_indices, len(arr))
 
     # the coords and offsets into the coordinates of the linestrings
     coords, indices = get_coordinates(arr_flat, return_index=True)
@@ -53,7 +74,7 @@ def _get_arrays_polygon(arr):
     # explode/flatten the Polygons into Rings
     arr_flat, ring_indices = get_rings(arr, return_index=True)
     # the offsets into the exterior/interior rings of the multipolygon parts
-    offsets2 = np.insert(np.bincount(ring_indices).cumsum(), 0, 0)
+    offsets2 = _indices_to_offsets(ring_indices, len(arr))
 
     # the coords and offsets into the coordinates of the rings
     coords, indices = get_coordinates(arr_flat, return_index=True)
@@ -66,7 +87,7 @@ def _get_arrays_multipolygon(arr):
     # explode/flatten the MultiPolygons
     arr_flat, part_indices = get_parts(arr, return_index=True)
     # the offsets into the multipolygon parts
-    offsets3 = np.insert(np.bincount(part_indices).cumsum(), 0, 0)
+    offsets3 = _indices_to_offsets(part_indices, len(arr))
 
     # explode/flatten the Polygons into Rings
     arr_flat2, ring_indices = get_rings(arr_flat, return_index=True)
@@ -80,7 +101,7 @@ def _get_arrays_multipolygon(arr):
     return coords.ravel(), (offsets1, offsets2, offsets3)
 
 
-def to_coordinates_offsets(arr):
+def to_ragged_array(arr):
     """
     Converts to a flat array of coordinates and offset arrays.
 
@@ -161,7 +182,12 @@ def _multipoint_from_flatcoords(coords, offsets):
     # recreate multipoints
     multipoint_parts = np.diff(offsets)
     multipoint_indices = np.repeat(np.arange(len(multipoint_parts)), multipoint_parts)
-    result = creation.multipoints(points, indices=multipoint_indices)
+
+    result = np.empty(len(offsets) - 1, dtype=object)
+    result = creation.multipoints(points, indices=multipoint_indices, out=result)
+    result[multipoint_parts == 0] = creation.empty(
+        1, geom_type=GeometryType.MULTIPOINT
+    ).item()
 
     return result
 
@@ -170,8 +196,14 @@ def _linestring_from_flatcoords(coords, offsets):
     # recreate linestrings
     linestring_n = np.diff(offsets)
     linestring_indices = np.repeat(np.arange(len(linestring_n)), linestring_n)
-    result = creation.linestrings(coords.reshape(-1, 2), indices=linestring_indices)
 
+    result = np.empty(len(offsets) - 1, dtype=object)
+    result = creation.linestrings(
+        coords.reshape(-1, 2), indices=linestring_indices, out=result
+    )
+    result[linestring_n == 0] = creation.empty(
+        1, geom_type=GeometryType.LINESTRING
+    ).item()
     return result
 
 
@@ -184,7 +216,14 @@ def _multilinestrings_from_flatcoords(coords, offsets1, offsets2):
     multilinestring_indices = np.repeat(
         np.arange(len(multilinestring_parts)), multilinestring_parts
     )
-    result = creation.multilinestrings(linestrings, indices=multilinestring_indices)
+
+    result = np.empty(len(offsets2) - 1, dtype=object)
+    result = creation.multilinestrings(
+        linestrings, indices=multilinestring_indices, out=result
+    )
+    result[multilinestring_parts == 0] = creation.empty(
+        1, geom_type=GeometryType.MULTILINESTRING
+    ).item()
 
     return result
 
@@ -198,7 +237,11 @@ def _polygon_from_flatcoords(coords, offsets1, offsets2):
     # recreate polygons
     polygon_rings_n = np.diff(offsets2)
     polygon_indices = np.repeat(np.arange(len(polygon_rings_n)), polygon_rings_n)
-    result = creation.polygons(rings, indices=polygon_indices)
+    result = np.empty(len(offsets2) - 1, dtype=object)
+    result = creation.polygons(rings, indices=polygon_indices, out=result)
+    result[polygon_rings_n == 0] = creation.empty(
+        1, geom_type=GeometryType.POLYGON
+    ).item()
 
     return result
 
@@ -212,12 +255,16 @@ def _multipolygons_from_flatcoords(coords, offsets1, offsets2, offsets3):
     multipolygon_indices = np.repeat(
         np.arange(len(multipolygon_parts)), multipolygon_parts
     )
-    result = creation.multipolygons(polygons, indices=multipolygon_indices)
+    result = np.empty(len(offsets3) - 1, dtype=object)
+    result = creation.multipolygons(polygons, indices=multipolygon_indices, out=result)
+    result[multipolygon_parts == 0] = creation.empty(
+        1, geom_type=GeometryType.MULTIPOLYGON
+    ).item()
 
     return result
 
 
-def from_coordinates_offsets(typ, coords, offsets=None):
+def from_ragged_array(typ, coords, offsets=None):
     """
     Creates geometries from a flat array of coordinates and offset arrays.
 
