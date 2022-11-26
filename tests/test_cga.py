6ecde4ad31b4ba3052a334834937e148ab97ec53
Phil Chiu <whophil@users.noreply.github.com>
2022-11-26 12:58:24 -0700

    PERF: vectorized implementation for signed area (#1323)

--- tests/test_cga.py
+++ tests/test_cga.py
@@ -1,5 +1,18 @@
 from . import unittest
-from shapely.geometry.polygon import LinearRing, orient, Polygon
+import pytest
+from shapely.geometry.polygon import LinearRing, orient, Polygon, signed_area
+
+
+class SignedAreaTestCase(unittest.TestCase):
+    def test_triangle(self):
+        tri = LinearRing([(0, 0), (2, 5), (7, 0)])
+        assert signed_area(tri) == pytest.approx(-7 * 5 / 2)
+
+    def test_square(self):
+        xmin, xmax = (-1, 1)
+        ymin, ymax = (-2, 3)
+        rect = LinearRing([(xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax), (xmin, ymin)])
+        assert signed_area(rect) == pytest.approx(10.)
 
 
 class RingOrientationTestCase(unittest.TestCase):
