f2fa5ae6a7ffc3fb6d5a33c8ff3f932c3c2f9885
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-08-08 21:47:30 +0200

    DOC: Clarification in doc of overlaps

--- shapely/predicates.py
+++ shapely/predicates.py
@@ -781,9 +781,9 @@ def intersects(a, b, **kwargs):
 def overlaps(a, b, **kwargs):
     """Returns True if A and B spatially overlap.
 
-    A and B overlap if they have some but not all points in common, have the
-    same dimension, and the intersection of the interiors of the two geometries
-    has the same dimension as the geometries themselves.  That is, only polyons
+    A and B overlap if they have some but not all points/portion of space in common,
+    have the same dimension, and the intersection of the interiors of the two geometries
+    has the same dimension as the geometries themselves. That is, only polyons
     can overlap other polygons and only lines can overlap other lines.
 
     If either A or B are None, the output is always False.
