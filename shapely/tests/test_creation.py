c6633718670a25c1380578cf7d2d8d3476e0cb3c
Casper van der Wel <caspervdw@gmail.com>
2023-09-21 20:19:19 +0200

    Port tests from #1811

--- shapely/tests/test_creation.py
+++ shapely/tests/test_creation.py
@@ -55,10 +55,57 @@ def test_points_invalid_ndim():
         shapely.points([0])
 
 
-@pytest.mark.skipif(shapely.geos_version < (3, 10, 0), reason="GEOS < 3.10")
-def test_points_nan_becomes_empty():
+@pytest.mark.skipif(
+    shapely.geos_version[:2] not in ((3, 10), (3, 11), (3, 12)),
+    reason="GEOS not in 3.10, 3.11, 3.12",
+)
+def test_points_nan_all_nan_becomes_empty():
     actual = shapely.points(np.nan, np.nan)
-    assert_geometries_equal(actual, shapely.Point())
+    assert actual.wkt == "POINT EMPTY"
+
+
+@pytest.mark.skipif(
+    shapely.geos_version[:2] not in ((3, 10), (3, 11)),
+    reason="GEOS not in 3.10, 3.11",
+)
+def test_points_nan_3D_all_nan_becomes_empty_2D():
+    actual = shapely.points(np.nan, np.nan, np.nan)
+    assert actual.wkt == "POINT EMPTY"
+
+
+@pytest.mark.skipif(shapely.geos_version[:2] != (3, 12), reason="GEOS != 3.12")
+def test_points_nan_3D_all_nan_becomes_empty():
+    actual = shapely.points(np.nan, np.nan, np.nan)
+    assert actual.wkt == "POINT Z EMPTY"
+
+
+@pytest.mark.skipif(shapely.geos_version < (3, 12, 0), reason="GEOS < 3.12")
+@pytest.mark.parametrize(
+    "coords,expected_wkt",
+    [
+        pytest.param(
+            [np.nan, np.nan],
+            "POINT (NaN NaN)",
+            marks=pytest.mark.skipif(
+                shapely.geos_version < (3, 13, 0), reason="GEOS < 3.13"
+            ),
+        ),
+        pytest.param(
+            [np.nan, np.nan, np.nan],
+            "POINT Z (NaN NaN NaN)",
+            marks=pytest.mark.skipif(
+                shapely.geos_version < (3, 13, 0), reason="GEOS < 3.13"
+            ),
+        ),
+        ([1, np.nan], "POINT (1 NaN)"),
+        ([np.nan, 1], "POINT (NaN 1)"),
+        ([np.nan, 1, np.nan], "POINT Z (NaN 1 NaN)"),
+        ([np.nan, np.nan, 1], "POINT Z (NaN NaN 1)"),
+    ],
+)
+def test_points_handle_nan_allow(coords, expected_wkt):
+    actual = shapely.points(coords)
+    assert actual.wkt == expected_wkt
 
 
 def test_linestrings_from_coords():
