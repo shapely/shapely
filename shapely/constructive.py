02f17f2d09f69985cafd0d0e2af87d490fd3cf8f
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-12-04 23:30:04 +0100

    Add missing spaces in docstring

--- shapely/constructive.py
+++ shapely/constructive.py
@@ -514,13 +514,13 @@ def make_valid(geometry, method="linework", keep_collapsed=True, **kwargs):
     Parameters
     ----------
     geometry : Geometry or array_like
-    method:  {'linework', 'structure'}, default 'linework'
+    method : {'linework', 'structure'}, default 'linework'
         Algorithm to use when repairing geometry. The linework algorithm combines all
         rings into a set of noded lines and then extracts valid polygons from that
         linework. The structure algorithm first makes all rings valid, then merges
         shells and subtracts holes from shells to generate valid result. It assumes that
         holes and shells are correctly categorized. 'structure' requires GEOS >= 3.10.
-    keep_collapsed: bool, default True
+    keep_collapsed : bool, default True
         For the 'structure' method, True will keep components that have collapsed into a
         lower dimensionality. For example, a ring collapsing to a line, or a line
         collapsing to a point. Must be True for the 'linework' method.
