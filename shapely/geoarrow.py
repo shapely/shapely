bb1da2041e6bdf161d7399376dff74add6e5ca57
Dewey Dunnington <dewey@fishandwhistle.net>
2024-03-16 22:51:12 -0300

    use more capsule stuff

--- shapely/geoarrow.py
+++ shapely/geoarrow.py
@@ -18,84 +18,51 @@ class Encoding(enum.Enum):
     GEOARROW_INTERLEAVED = SchemaCalculator.ENCODING_GEOARROW_INTERLEAVED
 
 
-def type_pyarrow(encoding, geometry_type=None, dimensions=None):
-    import pyarrow as pa
+class GeoArrowTypeExporter:
 
-    encoding = Encoding(encoding).value
-
-    if geometry_type == GeometryType.POINT:
-        wkb_type = 1
-    elif geometry_type in (GeometryType.LINESTRING, GeometryType.LINEARRING):
-        wkb_type = 2
-    elif geometry_type == GeometryType.POLYGON:
-        wkb_type = 3
-    elif geometry_type == GeometryType.MULTIPOINT:
-        wkb_type = 4
-    elif geometry_type == GeometryType.MULTILINESTRING:
-        wkb_type = 5
-    elif geometry_type == GeometryType.MULTIPOLYGON:
-        wkb_type = 6
-    elif geometry_type == GeometryType.GEOMETRYCOLLECTION:
-        wkb_type = 7
-    else:
+    def __init__(self, encoding, geometry_type=None, dimensions=None):
+        encoding = Encoding(encoding).value
         wkb_type = 0
 
-    if dimensions is not None:
-        dimensions = dimensions.lower()
+        if geometry_type == GeometryType.POINT:
+            wkb_type = 1
+        elif geometry_type in (GeometryType.LINESTRING, GeometryType.LINEARRING):
+            wkb_type = 2
+        elif geometry_type == GeometryType.POLYGON:
+            wkb_type = 3
+        elif geometry_type == GeometryType.MULTIPOINT:
+            wkb_type = 4
+        elif geometry_type == GeometryType.MULTILINESTRING:
+            wkb_type = 5
+        elif geometry_type == GeometryType.MULTIPOLYGON:
+            wkb_type = 6
+        elif geometry_type == GeometryType.GEOMETRYCOLLECTION:
+            wkb_type = 7
+        elif geometry_type is not None:
+            raise ValueError(f"Unknown value for geometry_type: '{geometry_type}'")
 
-    if dimensions == "xy" or dimensions is None:
-        wkb_type += 0
-    elif dimensions == "xyz":
-        wkb_type += 1000
-    elif dimensions == "xym":
-        wkb_type += 2000
-    elif dimensions == "xyzm":
-        wkb_type += 3000
+        if dimensions is not None:
+            dimensions = dimensions.lower()
 
-    capsule = SchemaCalculator.from_wkb_type(encoding, wkb_type)
-    return pa.DataType._import_from_c_capsule(capsule)
+        if dimensions == "xy" or dimensions is None:
+            wkb_type += 0
+        elif dimensions == "xyz":
+            wkb_type += 1000
+        elif dimensions == "xym":
+            wkb_type += 2000
+        elif dimensions == "xyzm":
+            wkb_type += 3000
+        else:
+            raise ValueError(f"Unknown value for dimensions: '{dimensions}'")
 
+        self._encoding = encoding
+        self._wkb_type = wkb_type
 
-def infer_pyarrow_type(obj, encoding=None):
-    import pyarrow as pa
-
-    if encoding is None:
-        encoding = Encoding.GEOARROW.value
-    else:
-        encoding = Encoding(encoding).value
-
-    # Note: faster to iterate over np.array(obj)
-    obj = np.array(obj)
-
-    calculator = SchemaCalculator()
-    calculator.ingest_geometry(obj)
-    capsule = calculator.finish(encoding)
-
-    return pa.DataType._import_from_c_capsule(capsule)
-
-
-def to_pyarrow(obj, preferred_encoding=None):
-    import pyarrow as pa
-
-    exporter = GeoArrowExporter(obj, preferred_encoding=preferred_encoding)
-    return pa.array(exporter)
+    def __arrow_c_schema__(self):
+        return SchemaCalculator.from_wkb_type(self._encoding, self._wkb_type)
 
 
-def from_arrow(arrays, schema, n=None):
-    schema = schema.__arrow_c_schema__()
-    reader = ArrayReader(schema)
-
-    if n is not None:
-        reader.reserve(n)
-
-    for array in arrays:
-        _, array = array.__arrow_c_array__()
-        reader.read(array)
-
-    return reader.finish()
-
-
-class GeoArrowExporter:
+class GeoArrowArrayExporter:
 
     def __init__(self, geometries, *, preferred_encoding=None) -> None:
         self._geometries = np.array(geometries)
@@ -144,3 +111,44 @@ class GeoArrowExporter:
             na_chunks, na_schema, validate=False, move=True
         )
         return na_stream.__arrow_c_stream__()
+
+
+def type_pyarrow(encoding, geometry_type=None, dimensions=None):
+    import pyarrow as pa
+
+    exporter = GeoArrowTypeExporter(encoding, geometry_type, dimensions)
+    return pa.DataType._import_from_c_capsule(exporter.__arrow_c_schema__())
+
+
+def infer_pyarrow_type(obj, encoding=None):
+    import pyarrow as pa
+
+    if encoding is None:
+        encoding = Encoding.GEOARROW.value
+    else:
+        encoding = Encoding(encoding).value
+
+    exporter = GeoArrowArrayExporter(obj, preferred_encoding=encoding)
+    return pa.DataType._import_from_c_capsule(exporter.__arrow_c_schema__())
+
+
+def to_pyarrow(obj, preferred_encoding=None):
+    import pyarrow as pa
+
+    exporter = GeoArrowArrayExporter(obj, preferred_encoding=preferred_encoding)
+    return pa.array(exporter)
+
+
+def from_arrow(arrays, schema, n=None):
+
+    schema = schema.__arrow_c_schema__()
+    reader = ArrayReader(schema)
+
+    if n is not None:
+        reader.reserve(n)
+
+    for array in arrays:
+        _, array = array.__arrow_c_array__()
+        reader.read(array)
+
+    return reader.finish()
