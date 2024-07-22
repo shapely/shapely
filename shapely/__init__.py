d3c865b7a9cde7ebcd2a22b8af631f6212f4a403
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-07-23 00:36:35 +0200

    Move import of GEOSException to the top

--- shapely/__init__.py
+++ shapely/__init__.py
@@ -1,3 +1,4 @@
+from shapely.lib import GEOSException  # noqa: F401, I001
 from shapely import _version
 from shapely._geometry import *  # noqa: F403
 from shapely.constructive import *  # noqa: F403
@@ -19,7 +20,6 @@ from shapely.geometry import (
 from shapely.io import *  # noqa: F403
 from shapely.lib import (
     Geometry,  # noqa: F401
-    GEOSException,  # noqa: F401
     geos_capi_version,  # noqa: F401
     geos_capi_version_string,  # noqa: F401
     geos_version,  # noqa: F401
