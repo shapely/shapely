6823a505fb345409e9d39bea22cc38172c6064da
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-09-13 16:10:48 +0200

    update test for correct result in recent GEOS

--- shapely/tests/legacy/test_polylabel.py
+++ shapely/tests/legacy/test_polylabel.py
@@ -2,6 +2,7 @@ import unittest
 
 import pytest
 
+import shapely
 from shapely.algorithms.polylabel import polylabel
 from shapely.geometry import LineString, Point, Polygon
 
@@ -47,10 +48,12 @@ class PolylabelTestCase(unittest.TestCase):
             ]
         )
         label = polylabel(polygon)
-        # correct result would be:
-        # assert label.coords[:] == [(32.722025, -117.201875)]
-        # the upstream GEOS implementation does not
-        assert label.coords[:] == [(32.722025, -117.208595)]
+        if shapely.geos_version >= (3, 12, 0):
+            # recent GEOS corrects for this
+            assert label.coords[:] == [(32.722025, -117.201875)]
+        else:
+            # older versions not
+            assert label.coords[:] == [(32.722025, -117.208595)]
 
     def test_polygon_with_hole(self):
         """
