ecda8309ef824dae9d6fb1712f8c31c209ed2d27
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-09-13 17:54:07 +0200

    DEPR: deprecate the shapely.geos module

--- shapely/geos.py
+++ shapely/geos.py
@@ -1,6 +1,16 @@
 """Proxies for libgeos, GEOS-specific exceptions, and utilities."""
 
+import warnings
+
 import shapely
+
+warnings.warn(
+    "The 'shapely.geos' module is deprecated, and will be removed in a future version. "
+    "All attributes of 'shapely.geos' are now available directly from the top-level "
+    "'shapely' namespace.",
+    DeprecationWarning,
+    stacklevel=2,
+)
 
 geos_version_string = shapely.geos_capi_version_string
 geos_version = shapely.geos_version
