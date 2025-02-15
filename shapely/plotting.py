09924ad12425a32fa84f1347c4ced9cb62633931
Mathias Hauser <mathause@users.noreply.github.com>
2025-02-15 11:28:00 +0100

    BUG: ensure interior is not filled in plot_polygon (#1933)
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- shapely/plotting.py
+++ shapely/plotting.py
@@ -22,11 +22,14 @@ def _default_ax():
 def _path_from_polygon(polygon):
     from matplotlib.path import Path
 
+    from shapely.ops import orient
+
     if isinstance(polygon, shapely.MultiPolygon):
         return Path.make_compound_path(
             *[_path_from_polygon(poly) for poly in polygon.geoms]
         )
     else:
+        polygon = orient(polygon)
         return Path.make_compound_path(
             Path(np.asarray(polygon.exterior.coords)[:, :2]),
             *[Path(np.asarray(ring.coords)[:, :2]) for ring in polygon.interiors],
