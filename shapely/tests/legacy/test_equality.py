c3164cba7d2c5e083effa33e8de2a0871bd40200
Mike Taves <mwtoews@gmail.com>
2025-02-28 21:21:12 +1300

    DEPR: remove BaseGeometry.almost_equals()

--- shapely/tests/legacy/test_equality.py
+++ shapely/tests/legacy/test_equality.py
@@ -1,7 +1,6 @@
 import pytest
 
 from shapely import Point, Polygon
-from shapely.errors import ShapelyDeprecationWarning
 
 
 def test_equals_exact():
@@ -27,21 +26,9 @@ def test_equals_exact():
     assert not p1.equals_exact(p3, 1e-5)
 
 
-def test_almost_equals_default():
-    p1 = Point(1.0, 1.0)
-    p2 = Point(1.0 + 1e-7, 1.0 + 1e-7)  # almost equal to 6 places
-    p3 = Point(1.0 + 1e-6, 1.0 + 1e-6)  # not almost equal
-    with pytest.warns(ShapelyDeprecationWarning):
-        assert p1.almost_equals(p2)
-    with pytest.warns(ShapelyDeprecationWarning):
-        assert not p1.almost_equals(p3)
-
-
-def test_almost_equals():
+def test_almost_equals_removed():
+    # deprecated shapely 2.0, removed 2.1
     p1 = Point(1.0, 1.0)
     p2 = Point(1.1, 1.1)
-    assert not p1.equals(p2)
-    with pytest.warns(ShapelyDeprecationWarning):
-        assert p1.almost_equals(p2, 0)
-    with pytest.warns(ShapelyDeprecationWarning):
-        assert not p1.almost_equals(p2, 1)
+    with pytest.raises(AttributeError):
+        assert p1.almost_equals(p2)
