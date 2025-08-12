6fd415b8fc19896f2d342b9192726190a05c06e4
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2025-08-12 10:47:43 +0200

    Update shapely/set_operations.py
    Co-authored-by: Dan Baston <dbaston@gmail.com>

--- shapely/set_operations.py
+++ shapely/set_operations.py
@@ -563,7 +563,7 @@ def coverage_union(a, b, **kwargs):
     This is an optimized version of union which assumes the polygons to be
     non-overlapping.
     If this assumption is not met, the exact result is not guaranteed
-    (depending on the GEOS version, it may return the input unchanges or raise
+    (depending on the GEOS version, it may return the input unchanged or raise
     an error).
 
     Parameters
