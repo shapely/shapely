c5d9a2fb596c4f5cac3f2bb61a3530ff7bfa66ac
JuhoErvasti <juho.ervasti@gispo.fi>
2026-06-25 00:25:45 +0000

    test(coverage-clean): normalize test results

--- shapely/tests/test_coverage.py
+++ shapely/tests/test_coverage.py
@@ -281,57 +281,61 @@ def test_coverage_clean_overlap():
 @pytest.mark.skipif(shapely.geos_version < (3, 14, 0), reason="requires >= 3.14")
 def test_coverage_clean_gap():
     # polygons with a gap
-    poly1 = shapely.from_wkt("POLYGON ((0 11, 10 11, 10 -1, 0 -1, 0 11))")
+    poly1 = shapely.from_wkt("POLYGON ((0 -1, 0 11, 10 11, 10 -1, 0 -1))")
     poly2 = shapely.from_wkt(
-        "POLYGON ((10 10, 10 13, 20 13, 20 -2, 10 -2, 10 0, 10.5 1, 10.5 8.5, 10 10))",
+        "POLYGON ((10 -2, 10 0, 10.5 1, 10.5 8.5, 10 10, 10 13, 20 13, 20 -2, 10 -2))",
     )
 
-    # gap width 0.0
+    # gap width 0.0 (should be the same as input)
     expected_1 = shapely.from_wkt(
         [
             "POLYGON ((0 -1, 0 11, 10 11, 10 10, 10 0, 10 -1, 0 -1))",
-            "POLYGON ((10 13, 20 13, 20 -2, 10 -2, 10 -1, 10 0, 10.5 1, "
-            "10.5 8.5, 10 10, 10 11, 10 13))",
+            "POLYGON ((10 -2, 10 -1, 10 0, 10.5 1, 10.5 8.5, 10 10, "
+            "10 11, 10 13, 20 13, 20 -2, 10 -2))",
         ]
     )
-    result_1 = shapely.coverage_clean([poly1, poly2], gap_width=0)
+    result_1 = shapely.normalize(
+        shapely.coverage_clean([poly1, poly2], gap_width=0),
+    )
     assert_geometries_equal(result_1, expected_1)
 
-    # gap width 1
+    # gap width 1 (gap should close)
     expected_2 = shapely.from_wkt(
         [
             "POLYGON ((0 -1, 0 11, 10 11, 10 10, 10 0, 10 -1, 0 -1))",
-            "POLYGON ((10 13, 20 13, 20 -2, 10 -2, 10 -1, 10 0, 10 10, 10 11, 10 13))",
+            "POLYGON ((10 -2, 10 -1, 10 0, 10 10, 10 11, 10 13, 20 13, 20 -2, 10 -2))",
         ]
     )
-    result_2 = shapely.coverage_clean([poly1, poly2], gap_width=1)
+    result_2 = shapely.normalize(shapely.coverage_clean([poly1, poly2], gap_width=1))
     assert_geometries_equal(result_2, expected_2)
 
 
 @pytest.mark.skipif(shapely.geos_version < (3, 14, 0), reason="requires >= 3.14")
 def test_coverage_clean_snapping():
     # polygons with a gap
-    poly1 = shapely.from_wkt("POLYGON ((0 11, 10 11, 10 -1, 0 -1, 0 11))")
+    poly1 = shapely.from_wkt("POLYGON ((0 -1, 0 11, 10 11, 10 -1, 0 -1))")
     poly2 = shapely.from_wkt(
-        "POLYGON ((10 10, 10 13, 20 13, 20 -2, 10 -2, 10 0, 10.5 1, 10.5 8.5, 10 10))",
+        "POLYGON ((10 -2, 10 0, 10.5 1, 10.5 8.5, 10 10, 10 13, 20 13, 20 -2, 10 -2))",
     )
 
-    # snapping distance 0.0
+    # snapping distance 0.0 (should be the same as input)
     expected_1 = shapely.from_wkt(
         [
             "POLYGON ((0 -1, 0 11, 10 11, 10 10, 10 0, 10 -1, 0 -1))",
-            "POLYGON ((10 13, 20 13, 20 -2, 10 -2, 10 -1, 10 0, 10.5 1, "
-            "10.5 8.5, 10 10, 10 11, 10 13))",
+            "POLYGON ((10 -2, 10 -1, 10 0, 10.5 1, 10.5 8.5, 10 10, 10 "
+            "11, 10 13, 20 13, 20 -2, 10 -2))",
         ]
     )
-    result_1 = shapely.coverage_clean([poly1, poly2], gap_width=0, snapping_distance=0)
+    result_1 = shapely.normalize(
+        shapely.coverage_clean([poly1, poly2], gap_width=0, snapping_distance=0),
+    )
     assert_geometries_equal(result_1, expected_1)
 
-    # snapping distance 2.0
+    # snapping distance 2.0 (vertices should be snapped)
     expected_2 = shapely.from_wkt(
         [
-            "POLYGON ((0 -1, 0 11, 10 11, 10.5 8.5, 10.5 1, 10 -1, 0 -1))",
-            "POLYGON ((20 13, 20 -2, 10 -1, 10.5 1, 10.5 8.5, 10 11, 20 13))",
+            "POLYGON ((10.5 8.5, 10.5 1, 10 -1, 0 -1, 0 11, 10 11, 10.5 8.5))",
+            "POLYGON ((10.5 1, 10.5 8.5, 10 11, 20 13, 20 -2, 10 -1, 10.5 1))",
         ]
     )
     result_2 = shapely.coverage_clean([poly1, poly2], gap_width=1, snapping_distance=2)
@@ -343,57 +347,65 @@ def test_coverage_clean_merge_strategies():
     # overlapping polygons
     poly1 = shapely.from_wkt("POLYGON ((0 0, 0 10, 10 10, 10 0, 0 0))")
     poly2 = shapely.from_wkt("POLYGON ((9 0, 9 21, 20 21, 20 0, 9 0))")
-    poly3 = shapely.from_wkt("POLYGON ((10 14, 6 14, 6 10, 10 10, 10 14))")
+    poly3 = shapely.from_wkt("POLYGON ((6 10, 6 14, 10 14, 10 10, 6 10))")
 
     expected_longest_border = shapely.from_wkt(
         [
             "POLYGON ((0 0, 0 10, 6 10, 9 10, 10 10, 10 0, 9 0, 0 0))",
-            "POLYGON ((10 10, 9 10, 9 14, 9 21, 20 21, 20 0, 10 0, 10 10))",
+            "POLYGON ((9 10, 9 14, 9 21, 20 21, 20 0, 10 0, 10 10, 9 10))",
             "POLYGON ((6 10, 6 14, 9 14, 9 10, 6 10))",
         ]
     )
-    result_longest_border = shapely.coverage_clean(
-        [poly1, poly2, poly3],
-        merge_strategy="longest_border",
+    result_longest_border = shapely.normalize(
+        shapely.coverage_clean(
+            [poly1, poly2, poly3],
+            merge_strategy="longest_border",
+        )
     )
     assert_geometries_equal(result_longest_border, expected_longest_border)
 
     expected_max_area = shapely.from_wkt(
         [
-            "POLYGON ((9 0, 0 0, 0 10, 6 10, 9 10, 9 0))",
-            "POLYGON ((9 21, 20 21, 20 0, 10 0, 9 0, 9 10, 9 14, 9 21))",
+            "POLYGON ((0 0, 0 10, 6 10, 9 10, 9 0, 0 0))",
+            "POLYGON ((9 0, 9 10, 9 14, 9 21, 20 21, 20 0, 10 0, 9 0))",
             "POLYGON ((6 10, 6 14, 9 14, 9 10, 6 10))",
         ]
     )
-    result_max_area = shapely.coverage_clean(
-        [poly1, poly2, poly3],
-        merge_strategy="max_area",
+    result_max_area = shapely.normalize(
+        shapely.coverage_clean(
+            [poly1, poly2, poly3],
+            merge_strategy="max_area",
+        )
     )
     assert_geometries_equal(result_max_area, expected_max_area)
 
     expected_min_area = shapely.from_wkt(
         [
             "POLYGON ((0 0, 0 10, 6 10, 9 10, 10 10, 10 0, 9 0, 0 0))",
-            "POLYGON ((10 10, 10 14, 9 14, 9 21, 20 21, 20 0, 10 0, 10 10))",
+            "POLYGON ((9 14, 9 21, 20 21, 20 0, 10 0, 10 10, 10 14, 9 14))",
             "POLYGON ((6 10, 6 14, 9 14, 10 14, 10 10, 9 10, 6 10))",
         ]
     )
-    result_min_area = shapely.coverage_clean(
-        [poly1, poly2, poly3],
-        merge_strategy="min_area",
+    result_min_area = shapely.normalize(
+        shapely.coverage_clean(
+            [poly1, poly2, poly3],
+            merge_strategy="min_area",
+        )
     )
     assert_geometries_equal(result_min_area, expected_min_area)
 
     expected_min_index = shapely.from_wkt(
         [
             "POLYGON ((0 0, 0 10, 6 10, 9 10, 10 10, 10 0, 9 0, 0 0))",
-            "POLYGON ((10 10, 9 10, 9 14, 9 21, 20 21, 20 0, 10 0, 10 10))",
+            "POLYGON ((9 10, 9 14, 9 21, 20 21, 20 0, 10 0, 10 10, 9 10))",
             "POLYGON ((6 10, 6 14, 9 14, 9 10, 6 10))",
         ]
     )
-    result_min_index = shapely.coverage_clean(
-        [poly1, poly2, poly3],
-        merge_strategy="min_index",
+    result_min_index = shapely.normalize(
+        shapely.coverage_clean(
+            [poly1, poly2, poly3],
+            merge_strategy="min_index",
+        )
     )
     assert_geometries_equal(result_min_index, expected_min_index)
 
@@ -438,12 +450,12 @@ def test_coverage_clean_overlap_multipolygons():
 
     expected = shapely.from_wkt(
         [
-            "MULTIPOLYGON (((0.5 0, 0 0, 0 0.9, 0 1, 0.5 1, 1 1, 1.5 1, "
-            "1.5 0, 1 0, 0.5 0)), ((5 5, 5 6, 5.5 6, 6 6, 6 5, 5.5 5, 5 5)))",
+            "MULTIPOLYGON (((0 0, 0 0.9, 0 1, 0.5 1, 1 1, 1.5 1, 1.5 0, 1 0, 0.5 0, "
+            "0 0)), ((5 5, 5 6, 5.5 6, 6 6, 6 5, 5.5 5, 5 5)))",
             "MULTIPOLYGON (((0 1, 0 2, 1 2, 1 1, 0.5 1, 0 1)), "
-            "((6 6, 7 6, 7 5, 6 5, 6 6)))",
+            "((6 5, 6 6, 7 6, 7 5, 6 5)))",
         ],
     )
-    result = shapely.coverage_clean([multipoly1, multipoly2])
+    result = shapely.normalize(shapely.coverage_clean([multipoly1, multipoly2]))
 
     assert_geometries_equal(result, expected)
